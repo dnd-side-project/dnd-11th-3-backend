02e22e8bff66f9ce5180f37ba1efc09ed5c0fb6d
hs12 <loveanna1228@gmail.com>
2024-07-20 17:11:57 +0900

    [feat] : 프로젝트 세팅
    Co-authored-by: dudxo <father8475@naver.com>
    Co-authored-by: dohui-son <thsehgml37@korea.ac.kr>

---
+++ src/main/java/com/dnd/gongmuin/GongmuInApplication.java
@@ -0,0 +1,13 @@
+package com.dnd.gongmuin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GongmuInApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GongmuInApplication.class, args);
+	}
+
+}
