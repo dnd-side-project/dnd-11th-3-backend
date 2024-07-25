e5e41279c9d25e67acb384e99d1d364d8a4a47e7
dudxo <father8475@naver.com>
2024-07-25 22:48:03 +0900

    [test] : memberRepository.findBySocialEmail() 테스트 구현

---
+++ src/test/java/com/dnd/gongmuin/member/repository/MemberRepositoryTest.java
@@ -0,0 +1,49 @@
+package com.dnd.gongmuin.member.repository;
+
+import static com.dnd.gongmuin.member.domain.JobCategory.*;
+import static com.dnd.gongmuin.member.domain.JobGroup.*;
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.dnd.gongmuin.member.domain.Member;
+
+@Transactional
+@SpringBootTest
+class MemberRepositoryTest {
+
+	@Autowired
+	MemberRepository memberRepository;
+
+	@DisplayName("소셜이메일로 특정 회원을 조회한다.")
+	@Test
+	void test() {
+		// given
+		Member 공무인1 = createMember("공무인1", "kakao1234/영태", "gongmuin@nate.com", "gongumin@korea.kr");
+		Member savedMember = memberRepository.save(공무인1);
+
+		// when
+		Member findMember = memberRepository.findBySocialEmail("gongmuin@nate.com");
+
+		// then
+		assertThat(findMember.getNickname()).isEqualTo("공무인1");
+	}
+
+	private Member createMember(String nickname, String socialName, String socialEmail, String officialEmail) {
+		return Member.builder()
+			.nickname(nickname)
+			.socialName(socialName)
+			.socialEmail(socialEmail)
+			.officialEmail(officialEmail)
+			.jobCategory(GAS)
+			.jobGroup(ENGINEERING)
+			.credit(10000)
+			.build();
+
+	}
+
+}
