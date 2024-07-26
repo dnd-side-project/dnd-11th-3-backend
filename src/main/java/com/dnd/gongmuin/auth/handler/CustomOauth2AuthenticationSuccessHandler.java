60de8a7ba1a709e5e480aeca4cd621359ba4a5e6
dudxo <father8475@naver.com>
2024-07-27 01:53:25 +0900

    [feat] : OauthLogin 성공 핸들러 추가 - 회원 구분하여 신규 회원일시 추가 정보 기입 페이지로 리다이렉트 구현

---
+++ src/main/java/com/dnd/gongmuin/auth/handler/CustomOauth2AuthenticationSuccessHandler.java
@@ -0,0 +1,46 @@
+package com.dnd.gongmuin.auth.handler;
+
+import java.io.IOException;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+
+import com.dnd.gongmuin.auth.dto.CustomOauth2User;
+import com.dnd.gongmuin.auth.service.AuthService;
+import com.dnd.gongmuin.member.domain.Member;
+import com.dnd.gongmuin.member.repository.MemberRepository;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class CustomOauth2AuthenticationSuccessHandler implements AuthenticationSuccessHandler {
+
+	private final AuthService authService;
+	private final MemberRepository memberRepository;
+
+	@Override
+	public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
+		Authentication authentication) throws IOException, ServletException {
+
+		CustomOauth2User customOauth2User = (CustomOauth2User)authentication.getPrincipal();
+
+		String socialEmail = customOauth2User.getEmail();
+		Member findmember = memberRepository.findBySocialEmail(socialEmail).get();
+
+		if (!isAuthStatusOld(findmember)) {
+			response.sendRedirect("/additional-info");
+		} else {
+			// TODO : 기존회원 JWT 발급 및 페이지 리다이렉션 구현
+		}
+	}
+
+	private boolean isAuthStatusOld(Member member) {
+		return authService.isAuthStatusOld(member);
+	}
+
+}
