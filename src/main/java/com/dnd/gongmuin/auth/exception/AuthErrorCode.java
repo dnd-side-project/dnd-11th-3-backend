6435102a6ec9e05a24ece4f50b7cdc959118059b
dudxo <father8475@naver.com>
2024-07-27 01:49:30 +0900

    [feat] : AuthErrorCode 에러코드 추가

--- src/main/java/com/dnd/gongmuin/auth/exception/AuthErrorCode.java
+++ src/main/java/com/dnd/gongmuin/auth/exception/AuthErrorCode.java
@@ -9,7 +9,9 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum AuthErrorCode implements ErrorCode {
 
-	UNSUPPORTED_SOCIAL_LOGIN("해당 소셜 로그인은 지원되지 않습니다.", "AUHT_001");
+	UNSUPPORTED_SOCIAL_LOGIN("해당 소셜 로그인은 지원되지 않습니다.", "AUHT_001"),
+	NOT_FOUND_PROVIDER("알맞은 Provider를 찾을 수 없습니다.", "AUTH_002"),
+	NOT_FOUND_AUTH("회원의 AUTH를 찾을 수 없습니다.", "AUTH_003");
 
 	private final String message;
 	private final String code;
