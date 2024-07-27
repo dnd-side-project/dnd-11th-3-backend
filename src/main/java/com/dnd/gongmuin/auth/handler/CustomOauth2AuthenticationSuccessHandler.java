c6c62e9bfeae6f37d4fe385cb8adad7488962ff7
dudxo <father8475@naver.com>
2024-07-27 20:11:02 +0900

    [fix] : 회원 검색 실패 시 예외 추가

--- src/main/java/com/dnd/gongmuin/auth/handler/CustomOauth2AuthenticationSuccessHandler.java
+++ src/main/java/com/dnd/gongmuin/auth/handler/CustomOauth2AuthenticationSuccessHandler.java
@@ -8,7 +8,9 @@ import org.springframework.stereotype.Component;
 
 import com.dnd.gongmuin.auth.dto.CustomOauth2User;
 import com.dnd.gongmuin.auth.service.AuthService;
+import com.dnd.gongmuin.common.exception.runtime.NotFoundException;
 import com.dnd.gongmuin.member.domain.Member;
+import com.dnd.gongmuin.member.exception.MemberErrorCode;
 import com.dnd.gongmuin.member.repository.MemberRepository;
 
 import jakarta.servlet.ServletException;
@@ -30,12 +32,14 @@ public class CustomOauth2AuthenticationSuccessHandler implements AuthenticationS
 		CustomOauth2User customOauth2User = (CustomOauth2User)authentication.getPrincipal();
 
 		String socialEmail = customOauth2User.getEmail();
-		Member findmember = memberRepository.findBySocialEmail(socialEmail).get();
+		Member findmember = memberRepository.findBySocialEmail(socialEmail)
+			.orElseThrow(() -> new NotFoundException(MemberErrorCode.NOT_FOUND_MEMBER));
 
 		if (!isAuthStatusOld(findmember)) {
 			response.sendRedirect("/additional-info");
 		} else {
 			// TODO : 기존회원 JWT 발급 및 페이지 리다이렉션 구현
+			response.sendRedirect("/");
 		}
 	}
 
