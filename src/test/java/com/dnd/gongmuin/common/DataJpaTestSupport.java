0fb530d302773b4d7f63f3b6f7a6738e65f1403d
hs12 <loveanna1228@gmail.com>
2024-07-29 19:36:53 +0900

    [test] : repository 테스트용 추상 클래스 추가

---
+++ src/test/java/com/dnd/gongmuin/common/DataJpaTestSupport.java
@@ -0,0 +1,14 @@
+package com.dnd.gongmuin.common;
+
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+
+import com.dnd.gongmuin.config.TestAuditingConfig;
+
+//repository용
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@Import(TestAuditingConfig.class)
+public abstract class DataJpaTestSupport extends TestContainerSupport{
+}
