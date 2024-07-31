cc8e067411f6e1454911e61f243a36e0aa424287
Son Gahyun <77109954+hyun2371@users.noreply.github.com>
2024-07-31 14:12:24 +0900

    [chore] : 테스트 환경 세팅 (#7)
    * [chore] : 테스트 관련 의존성 추가
    
    * [test] : 테스트 컨테이너 설정 파일 추가
    
    * [test] : repository 테스트용 추상 클래스 추가
    
    * [test] : controller 통합 테스트용 추상클래스 추가
    
    * [rename] : test support 클래스들 파일 경로 수정
    
    * [test] : testFixture 디렉토리 추가
    
    * [chore] : 빌드를 위한 dockerfile 추가

---
+++ src/test/java/com/dnd/gongmuin/common/fixture/QuestionPostFixture.java
@@ -0,0 +1,9 @@
+package com.dnd.gongmuin.common.fixture;
+
+import static lombok.AccessLevel.*;
+
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = PRIVATE)
+public class QuestionPostFixture {
+}
