900c15d9abb5922b686f66f262bce827e5c9229f
hs12 <loveanna1228@gmail.com>
2024-07-31 14:38:58 +0900

    [feat] : 파일 오류 메시지 수정

--- src/main/java/com/dnd/gongmuin/s3/exception/S3ErrorCode.java
+++ src/main/java/com/dnd/gongmuin/s3/exception/S3ErrorCode.java
@@ -9,11 +9,10 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum S3ErrorCode implements ErrorCode {
 	EMPTY_FILE_NAME("원본 파일명은 필수입니다.", "S3_001"),
+	INVALID_FILE_EXTENSION("잘못된 되었거나 지원하지 않는 파일 형식입니다.", "S3_002"),
 
-	UNSUPPORTED_FILE_TYPE("지원하지 않는 파일 형식입니다.", "S3_002"),
+	FAILED_TO_UPLOAD("파일을 업로드하는데 실패했습니다.", "S3_003");
 
-	FAILED_TO_UPLOAD("s3에 파일을 업로드하는데 실패했습니다.", "S3_003"),
-	INVALID_FILE_EXTENSION("파일 형식이 올바르지 않습니다.", "S4_004");
 
 	private final String message;
 	private final String code;
