9234f33618b0816c23cdfbccf890667e084001b1
dudxo <father8475@naver.com>
2024-08-01 21:13:39 +0900

    [feat] : jwt 검증 필터 구현

---
+++ src/main/java/com/dnd/gongmuin/security/jwt/util/TokenAuthenticationFilter.java
@@ -0,0 +1,54 @@
+package com.dnd.gongmuin.security.jwt.util;
+
+import static org.springframework.http.HttpHeaders.*;
+
+import java.io.IOException;
+import java.util.Date;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.util.ObjectUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Component
+public class TokenAuthenticationFilter extends OncePerRequestFilter {
+
+	private final TokenProvider tokenProvider;
+	private static final String TOKEN_PREFIX = "Bearer ";
+
+	@Override
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
+		FilterChain filterChain) throws ServletException, IOException {
+
+		String accessToken = resolveToken(request);
+
+		if (tokenProvider.validateToken(accessToken, new Date())) {
+			saveAuthentication(accessToken);
+		} else {
+			// TODO : 만료시 accessToken 재발급
+		}
+
+		filterChain.doFilter(request, response);
+	}
+
+	private void saveAuthentication(String accessToken) {
+		Authentication authentication = tokenProvider.getAuthentication(accessToken);
+		SecurityContextHolder.getContext().setAuthentication(authentication);
+	}
+
+	private String resolveToken(HttpServletRequest request) {
+		String token = request.getHeader(AUTHORIZATION);
+		if (ObjectUtils.isEmpty(token) || !token.startsWith(TOKEN_PREFIX)) {
+			return null;
+		}
+		return token.substring(TOKEN_PREFIX.length());
+	}
+}
