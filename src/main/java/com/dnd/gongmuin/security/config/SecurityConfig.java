c9d5ce7d1ec8474110bb03d43abcbc45ef70e46d
dudxo <father8475@naver.com>
2024-08-01 21:23:42 +0900

    [feat] : SecuritConfig 설정 추가 - WebSecurityCustomizer 빈 등록으로 특정 경로 보안 필터 우회 설정 - permitAll 경로 추가 - oauth2Login 실패 핸들러 추가 - 토큰 필터 추가 및 예외 EntiryPoint 추가

--- src/main/java/com/dnd/gongmuin/security/config/SecurityConfig.java
+++ src/main/java/com/dnd/gongmuin/security/config/SecurityConfig.java
@@ -3,10 +3,16 @@ package com.dnd.gongmuin.security.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-import com.dnd.gongmuin.security.handler.CustomOauth2AuthenticationSuccessHandler;
+import com.dnd.gongmuin.security.handler.CustomAuthenticationEntryPoint;
+import com.dnd.gongmuin.security.handler.CustomOauth2FailureHandler;
+import com.dnd.gongmuin.security.handler.CustomOauth2SuccessHandler;
+import com.dnd.gongmuin.security.jwt.util.TokenAuthenticationFilter;
+import com.dnd.gongmuin.security.jwt.util.TokenExceptionFilter;
 import com.dnd.gongmuin.security.service.CustomOauth2UserService;
 
 import lombok.RequiredArgsConstructor;
@@ -16,35 +22,49 @@ import lombok.RequiredArgsConstructor;
 public class SecurityConfig {
 
 	private final CustomOauth2UserService customOauth2UserService;
-	private final CustomOauth2AuthenticationSuccessHandler customOauth2AuthenticationSuccessHandler;
+	private final CustomOauth2SuccessHandler customOauth2SuccessHandler;
+	private final CustomOauth2FailureHandler customOauth2FailureHandler;
+	private final TokenAuthenticationFilter tokenAuthenticationFilter;
+
+	@Bean
+	public WebSecurityCustomizer webSecurityCustomizer() {
+		return web -> web.ignoring()
+			.requestMatchers("/error", "/favicon.ico");
+	}
 
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		http
+			.cors((auth) -> auth.disable())
 			.csrf((auth) -> auth.disable())
 			.formLogin((auth) -> auth.disable())
-			.httpBasic((auth) -> auth.disable());
-		http
-			// JWT 토큰 인증 기반을 위한 Session 정책 설정
+			.httpBasic((auth) -> auth.disable())
 			.sessionManagement(
 				(session) -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
 			);
+
 		http
 			.authorizeHttpRequests(
 				(auth) -> auth
-					.requestMatchers("/login/kakao").permitAll()
-					.requestMatchers("/additional-info").permitAll()
 					.requestMatchers("/").permitAll()
+					.requestMatchers("/api/auth/signin/kakao").permitAll()
+					.requestMatchers("/api/auth/member").permitAll()
+					.requestMatchers("/api/auth/check-email").permitAll()
 					.anyRequest().authenticated()
-			);
-		http
+			)
 			.oauth2Login((oauth2) -> oauth2
-				.successHandler(customOauth2AuthenticationSuccessHandler) // 로그인 성공 후 리디렉션할 URL
-				.failureUrl("/") // 로그인 실패 후 리디렉션할 URL
 				.userInfoEndpoint(
 					(userInfoEndpointConfig -> userInfoEndpointConfig.userService(customOauth2UserService))
 				)
-			);
+				.successHandler(customOauth2SuccessHandler)
+				.failureHandler(customOauth2FailureHandler)
+			)
+
+			.addFilterBefore(tokenAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+			.addFilterBefore(new TokenExceptionFilter(), tokenAuthenticationFilter.getClass())
+
+			.exceptionHandling((exception) -> exception
+				.authenticationEntryPoint(new CustomAuthenticationEntryPoint()));
 
 		return http.build();
 	}
