a1ab1ef23376fa651a48cdf033977d269dc5fa57
hs12 <loveanna1228@gmail.com>
2024-08-02 17:21:49 +0900

    [feat] : questionPost 등록 컨트롤러 코드 추가

---
+++ src/main/java/com/dnd/gongmuin/question_post/controller/QuestionPostController.java
@@ -0,0 +1,30 @@
+package com.dnd.gongmuin.question_post.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.dnd.gongmuin.question_post.dto.QuestionPostDetailResponse;
+import com.dnd.gongmuin.question_post.dto.RegisterQuestionPostRequest;
+import com.dnd.gongmuin.question_post.service.QuestionPostService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/question-posts")
+public class QuestionPostController {
+
+	private final QuestionPostService questionPostService;
+
+	@PostMapping
+	public ResponseEntity<QuestionPostDetailResponse> registerQuestionPost(
+		@RequestBody RegisterQuestionPostRequest request
+	) {
+		QuestionPostDetailResponse response = questionPostService.registerQuestionPost(request);
+		return ResponseEntity.ok(response);
+	}
+
+}
