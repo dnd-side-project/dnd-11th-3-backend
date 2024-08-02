fc4ba016d8c6eb1d74d8ca69ed7e1ae0661fd70e
dudxo <father8475@naver.com>
2024-08-02 22:10:56 +0900

    [style] Repository 코드 통일성을 위한 어노테이션 삭제

--- src/main/java/com/dnd/gongmuin/member/repository/MemberRepository.java
+++ src/main/java/com/dnd/gongmuin/member/repository/MemberRepository.java
@@ -3,11 +3,9 @@ package com.dnd.gongmuin.member.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import com.dnd.gongmuin.member.domain.Member;
 
-@Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
 	Optional<Member> findBySocialEmail(String socialEmail);
 
