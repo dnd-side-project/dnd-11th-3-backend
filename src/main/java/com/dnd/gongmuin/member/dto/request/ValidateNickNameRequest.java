90a4ee3a47cb3c74d5510dcdf0920296a55a8aff
dudxo <father8475@naver.com>
2024-08-03 00:13:07 +0900

    [feat] : requestDto validation 추가

--- src/main/java/com/dnd/gongmuin/member/dto/request/ValidateNickNameRequest.java
+++ src/main/java/com/dnd/gongmuin/member/dto/request/ValidateNickNameRequest.java
@@ -1,6 +1,11 @@
 package com.dnd.gongmuin.member.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 public record ValidateNickNameRequest(
+	@NotBlank(message = "닉네임은 필수 입력 항목입니다.")
+	@Size(min = 2, max = 12, message = "닉네임은 최소 2자리 이상 최대 12자 이하입니다.")
 	String nickname
 ) {
 
