d1895ff448712f97144bcd2939b79d2592db859a
hs12 <loveanna1228@gmail.com>
2024-08-02 17:21:06 +0900

    [feat] : DTO <-> 엔티티 변환QuestionPost Mapper 추가

---
+++ src/main/java/com/dnd/gongmuin/question_post/dto/QuestionPostMapper.java
@@ -0,0 +1,39 @@
+package com.dnd.gongmuin.question_post.dto;
+
+import java.util.List;
+
+import com.dnd.gongmuin.member.domain.JobGroup;
+import com.dnd.gongmuin.member.domain.Member;
+import com.dnd.gongmuin.question_post.domain.QuestionPost;
+import com.dnd.gongmuin.question_post.domain.QuestionPostImage;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class QuestionPostMapper {
+
+	public static QuestionPost toQuestionPost(RegisterQuestionPostRequest request, Member member) {
+		JobGroup jobGroup = JobGroup.of(request.jobGroup());
+		List<QuestionPostImage> images =request.imageUrls().stream()
+			.map(QuestionPostImage::from)
+			.toList();
+		return QuestionPost.of(request.title(), request.content(), 1000, jobGroup, images, member);
+	}
+
+	public static QuestionPostDetailResponse toQuestionPostDetailResponse(QuestionPost questionPost) {
+		Member member = questionPost.getMember();
+		return QuestionPostDetailResponse.of(
+			questionPost.getId(),
+			questionPost.getTitle(),
+			questionPost.getContent(),
+			questionPost.getImages().stream()
+				.map(QuestionPostImage::getImageUrl).toList(),
+			questionPost.getReward(),
+			questionPost.getJobGroup().getLabel(),
+			member.getId(),
+			member.getNickname(),
+			member.getJobGroup().getLabel()
+		);
+	}
+}
