53bd09874b209503c4cc110b50a13dec5bf76a88
hs12 <loveanna1228@gmail.com>
2024-08-03 12:47:55 +0900

    [test] : 제약 조건에 따른 memberFixture 수정

--- src/test/java/com/dnd/gongmuin/common/fixture/MemberFixture.java
+++ src/test/java/com/dnd/gongmuin/common/fixture/MemberFixture.java
@@ -2,6 +2,7 @@ package com.dnd.gongmuin.common.fixture;
 
 import static lombok.AccessLevel.*;
 
+import com.dnd.gongmuin.member.domain.JobCategory;
 import com.dnd.gongmuin.member.domain.JobGroup;
 import com.dnd.gongmuin.member.domain.Member;
 
@@ -13,6 +14,10 @@ public class MemberFixture {
 	public static Member member(){
 		return Member.builder()
 			.nickname("test")
+			.socialName("test")
+			.officialEmail("test@official.com")
+			.socialEmail("test@naver.com")
+			.jobCategory(JobCategory.of("가스"))
 			.jobGroup(JobGroup.of("공업"))
 			.build();
 	}
