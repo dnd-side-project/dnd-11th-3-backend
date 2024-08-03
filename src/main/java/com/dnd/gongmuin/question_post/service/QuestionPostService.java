7a8617bcf6b3270d13960e699dc531f494daafcf
hs12 <loveanna1228@gmail.com>
2024-08-03 16:04:03 +0900

    [feat] : 아이디로 질문글 조회 비즈니스 로직 작성

--- src/main/java/com/dnd/gongmuin/question_post/service/QuestionPostService.java
+++ src/main/java/com/dnd/gongmuin/question_post/service/QuestionPostService.java
@@ -3,12 +3,14 @@ package com.dnd.gongmuin.question_post.service;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.dnd.gongmuin.common.exception.runtime.NotFoundException;
 import com.dnd.gongmuin.member.domain.Member;
 import com.dnd.gongmuin.member.repository.MemberRepository;
 import com.dnd.gongmuin.question_post.domain.QuestionPost;
 import com.dnd.gongmuin.question_post.dto.QuestionPostDetailResponse;
 import com.dnd.gongmuin.question_post.dto.QuestionPostMapper;
 import com.dnd.gongmuin.question_post.dto.RegisterQuestionPostRequest;
+import com.dnd.gongmuin.question_post.exception.QuestionPostErrorCode;
 import com.dnd.gongmuin.question_post.repository.QuestionPostRepository;
 
 import jakarta.validation.ValidationException;
@@ -27,10 +29,16 @@ public class QuestionPostService {
 		return QuestionPostMapper.toQuestionPostDetailResponse(questionPostRepository.save(questionPost));
 	}
 
-	// TODO: 시큐리티 인증 객체로 대체
-	public Member getTempMember(){
-		return memberRepository.findById(3L).orElseThrow(ValidationException::new);
+	@Transactional(readOnly = true)
+	public QuestionPostDetailResponse getQuestionPostById(Long questionPostId) {
+		QuestionPost questionPost = questionPostRepository.findById(questionPostId)
+			.orElseThrow(() -> new NotFoundException(QuestionPostErrorCode.NOT_FOUND_QUESTION_POST));
+		return QuestionPostMapper.toQuestionPostDetailResponse(questionPost);
 	}
 
+	// TODO: 시큐리티 인증 객체로 대체
+	public Member getTempMember() {
+		return memberRepository.findById(3L).orElseThrow(ValidationException::new);
+	}
 
 }
