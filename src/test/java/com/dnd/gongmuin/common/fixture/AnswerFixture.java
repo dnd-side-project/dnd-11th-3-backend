2a1f92543403fc5fc225016ad9347a62056a4fa3
hs12 <loveanna1228@gmail.com>
2024-08-06 21:42:08 +0900

    [test] : AnswerFixture 객체 추가

--- src/test/java/com/dnd/gongmuin/common/fixture/AnswerFixture.java
+++ src/test/java/com/dnd/gongmuin/common/fixture/AnswerFixture.java
@@ -5,12 +5,22 @@ import java.time.LocalDateTime;
 import org.springframework.test.util.ReflectionTestUtils;
 
 import com.dnd.gongmuin.answer.domain.Answer;
+import com.dnd.gongmuin.member.domain.Member;
 
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class AnswerFixture {
+
+	public static Answer answer(Long questionPostId, Member member){
+		return Answer.of(
+			"답변 내용",
+			false,
+			questionPostId,
+			member
+		);
+	}
 
 	// 단위 테스트용
 	public static Answer answer(Long answerId, Long questionPostId){
