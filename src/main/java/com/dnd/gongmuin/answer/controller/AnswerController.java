b402979457541a74bd3b8db525a00131e0d32f17
hs12 <loveanna1228@gmail.com>
2024-08-06 13:35:12 +0900

    [feat] : 답변 등록 controller 함수 추가

---
+++ src/main/java/com/dnd/gongmuin/answer/controller/AnswerController.java
@@ -0,0 +1,33 @@
+package com.dnd.gongmuin.answer.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.dnd.gongmuin.answer.dto.AnswerDetailResponse;
+import com.dnd.gongmuin.answer.dto.RegisterAnswerRequest;
+import com.dnd.gongmuin.answer.service.AnswerService;
+import com.dnd.gongmuin.member.domain.Member;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/question-posts")
+public class AnswerController {
+	private final AnswerService answerService;
+
+	@PostMapping("/{questionPostId}/answers")
+	public ResponseEntity<AnswerDetailResponse> registerAnswer(
+		@PathVariable Long questionPostId,
+		@RequestBody RegisterAnswerRequest request,
+		@AuthenticationPrincipal Member member
+	) {
+		AnswerDetailResponse response = answerService.registerAnswer(questionPostId, request, member);
+		return ResponseEntity.ok(response);
+	}
+}
