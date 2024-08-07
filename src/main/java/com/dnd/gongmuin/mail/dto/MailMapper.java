14b1403f44b6fa8de72acc02a5274575e03f632a
dudxo <father8475@naver.com>
2024-08-07 22:07:53 +0900

    [feat] : 공무원 이메일 인증코드 검증 응답 DTO 추가(+Mapper)

--- src/main/java/com/dnd/gongmuin/mail/dto/MailMapper.java
+++ src/main/java/com/dnd/gongmuin/mail/dto/MailMapper.java
@@ -1,5 +1,6 @@
 package com.dnd.gongmuin.mail.dto;
 
+import com.dnd.gongmuin.mail.dto.response.AuthCodeResponse;
 import com.dnd.gongmuin.mail.dto.response.SendMailResponse;
 
 import lombok.AccessLevel;
@@ -10,5 +11,9 @@ public class MailMapper {
 
 	public static SendMailResponse toSendMailResponse(String toEmail) {
 		return new SendMailResponse(toEmail);
+	}
+
+	public static AuthCodeResponse toAuthCodeResponse(boolean result) {
+		return new AuthCodeResponse(result);
 	}
 }
---
+++ src/main/java/com/dnd/gongmuin/mail/dto/response/AuthCodeResponse.java
@@ -0,0 +1,6 @@
+package com.dnd.gongmuin.mail.dto.response;
+
+public record AuthCodeResponse(
+	boolean result
+) {
+}
