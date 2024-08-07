5cc9a99ff8462ab0493900d10f5b3dacf5445780
dudxo <father8475@naver.com>
2024-08-08 01:13:47 +0900

    [feat] : 만료 키 관련 에러코드 추가

--- src/main/java/com/dnd/gongmuin/redis/exception/RedisErrorCode.java
+++ src/main/java/com/dnd/gongmuin/redis/exception/RedisErrorCode.java
@@ -12,7 +12,8 @@ public enum RedisErrorCode implements ErrorCode {
 	REDIS_SAVE_ERROR("저장하지 못했습니다.", "REDIS_001"),
 	REDIS_FIND_ERROR("값을 찾는 도중 오류가 발생했습니다.", "REDIS_002"),
 	REDIS_DELETE_ERROR("삭제하지 못했습니다.", "REDIS_003"),
-	REDIS_EXPIRE_ERROR("만료시키지하지 못했습니다.", "REDIS_004");
+	REDIS_EXPIRE_ERROR("만료시키지하지 못했습니다.", "REDIS_004"),
+	REDIS_EXPIRED_ERROR("만료된 키 입니다.", "REDIS_005");
 
 	private final String message;
 	private final String code;
