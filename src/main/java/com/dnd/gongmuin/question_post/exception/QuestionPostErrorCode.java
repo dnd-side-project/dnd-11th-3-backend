39f6c4d7ef6fbc8176fc883c89977bb7667e40fd
hs12 <loveanna1228@gmail.com>
2024-08-07 15:08:30 +0900

    [feat] : 질문글 등록 리워드 검증 로직 추가

--- src/main/java/com/dnd/gongmuin/question_post/exception/QuestionPostErrorCode.java
+++ src/main/java/com/dnd/gongmuin/question_post/exception/QuestionPostErrorCode.java
@@ -9,7 +9,8 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum QuestionPostErrorCode implements ErrorCode {
 
-	NOT_FOUND_QUESTION_POST("해당 아이디의 질문 포스트가 존재하지 않습니다.", "QP_001");
+	NOT_FOUND_QUESTION_POST("해당 아이디의 질문 포스트가 존재하지 않습니다.", "QP_001"),
+	NOT_ENOUGH_CREDIT("보유한 크레딧이 부족합니다.", "QP_002");
 
 	private final String message;
 	private final String code;
