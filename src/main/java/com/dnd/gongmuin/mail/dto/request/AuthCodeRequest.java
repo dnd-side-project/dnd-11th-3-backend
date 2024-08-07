dc026be410ce3994d40bab1fd0dfad73c542582a
dudxo <father8475@naver.com>
2024-08-07 21:14:33 +0900

    [feat] : 인증코드 검증 request DTO 이메일 필드 추가

---
+++ src/main/java/com/dnd/gongmuin/mail/dto/request/AuthCodeRequest.java
@@ -0,0 +1,14 @@
+package com.dnd.gongmuin.mail.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+
+public record AuthCodeRequest(
+	@NotBlank(message = "인증 코드를 입력해주세요.")
+	@Pattern(regexp = "\\d{6}", message = "인증 코드는 6자리 숫자여야 합니다.")
+	String authCode,
+
+	@NotBlank(message = "공무원 이메일을 입력해주세요.")
+	String toEmail
+) {
+}
