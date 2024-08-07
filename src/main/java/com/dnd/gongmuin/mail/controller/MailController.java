f553dce8d4629fad993792d92e26fa6aba9ea3b9
dudxo <father8475@naver.com>
2024-08-07 23:56:43 +0900

    [fix] : 인증코드 요청 API HTTP Method 누락 추가

--- src/main/java/com/dnd/gongmuin/mail/controller/MailController.java
+++ src/main/java/com/dnd/gongmuin/mail/controller/MailController.java
@@ -3,6 +3,7 @@ package com.dnd.gongmuin.mail.controller;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,6 +24,7 @@ public class MailController {
 
 	private final MailService mailService;
 
+	@PostMapping
 	public ResponseEntity<SendMailResponse> sendToMail(
 		@RequestBody @Valid SendMailRequest sendMailRequest) {
 		SendMailResponse toEmail = mailService.sendEmail(sendMailRequest);
