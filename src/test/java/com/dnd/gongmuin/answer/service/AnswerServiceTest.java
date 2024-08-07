5a72cab6a6aee81283d0bcfd2774114126114e28
hs12 <loveanna1228@gmail.com>
2024-08-07 21:21:12 +0900

    [test] : 답변 채택 비즈니스 로직 테스트

--- src/test/java/com/dnd/gongmuin/answer/service/AnswerServiceTest.java
+++ src/test/java/com/dnd/gongmuin/answer/service/AnswerServiceTest.java
@@ -1,5 +1,6 @@
 package com.dnd.gongmuin.answer.service;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.mockito.BDDMockito.*;
 
 import java.util.List;
@@ -15,16 +16,20 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.SliceImpl;
+import org.springframework.test.util.ReflectionTestUtils;
 
 import com.dnd.gongmuin.answer.domain.Answer;
 import com.dnd.gongmuin.answer.dto.AnswerDetailResponse;
 import com.dnd.gongmuin.answer.dto.RegisterAnswerRequest;
 import com.dnd.gongmuin.answer.repository.AnswerRepository;
 import com.dnd.gongmuin.common.dto.PageResponse;
+import com.dnd.gongmuin.common.exception.runtime.ValidationException;
 import com.dnd.gongmuin.common.fixture.AnswerFixture;
 import com.dnd.gongmuin.common.fixture.MemberFixture;
 import com.dnd.gongmuin.common.fixture.QuestionPostFixture;
+import com.dnd.gongmuin.member.domain.Member;
 import com.dnd.gongmuin.question_post.domain.QuestionPost;
+import com.dnd.gongmuin.question_post.exception.QuestionPostErrorCode;
 import com.dnd.gongmuin.question_post.repository.QuestionPostRepository;
 
 @DisplayName("[AnswerService 테스트]")
@@ -86,5 +91,69 @@ class AnswerServiceTest {
 		Assertions.assertThat(response.content()).hasSize(2);
 		Assertions.assertThat(response.hasNext()).isFalse();
 		Assertions.assertThat(response.content().get(0).answerId()).isEqualTo(answer1.getId());
+	}
+
+	@DisplayName("[답변을 채택할 수 있다.]")
+	@Test
+	void chooseAnswer() {
+		//given
+		Long questionPostId = 1L;
+		Member member = MemberFixture.member(1L);
+		QuestionPost questionPost = QuestionPostFixture.questionPost(questionPostId, member);
+		Answer answer = AnswerFixture.answer(1L, questionPostId);
+
+		given(answerRepository.findById(answer.getId()))
+			.willReturn(Optional.of(answer));
+		given(questionPostRepository.findById(questionPost.getId()))
+			.willReturn(Optional.of(questionPost));
+
+		//when
+		AnswerDetailResponse response = answerService.chooseAnswer(answer.getId(), member);
+
+		//then
+		Assertions.assertThat(response.isChosen()).isTrue();
+	}
+
+	@DisplayName("[크레딧이 부족하면 답변을 채택할 수 없다.]")
+	@Test
+	void chooseAnswer_fail() {
+		//given
+		Long questionPostId = 1L;
+		Member member = MemberFixture.member(1L);
+		QuestionPost questionPost = QuestionPostFixture.questionPost(questionPostId, member);
+		ReflectionTestUtils.setField(questionPost, "reward", member.getCredit() + 1);
+		Answer answer = AnswerFixture.answer(1L, questionPostId);
+
+		given(answerRepository.findById(answer.getId()))
+			.willReturn(Optional.of(answer));
+		given(questionPostRepository.findById(questionPost.getId()))
+			.willReturn(Optional.of(questionPost));
+
+		//when & then
+		assertThatThrownBy(() -> answerService.chooseAnswer(answer.getId(), member))
+			.isInstanceOf(ValidationException.class)
+			.hasMessageContaining(QuestionPostErrorCode.NOT_ENOUGH_CREDIT.getMessage());
+
+	}
+
+	@DisplayName("[질문자가 아니면 채택할 수 없다.]")
+	@Test
+	void chooseAnswer_fail2() {
+		//given
+		Long questionPostId = 1L;
+		Member questioner = MemberFixture.member(1L);
+		Member notQuestioner = MemberFixture.member(2L);
+		QuestionPost questionPost = QuestionPostFixture.questionPost(questionPostId, questioner);
+		Answer answer = AnswerFixture.answer(1L, questionPostId);
+
+		given(answerRepository.findById(answer.getId()))
+			.willReturn(Optional.of(answer));
+		given(questionPostRepository.findById(questionPost.getId()))
+			.willReturn(Optional.of(questionPost));
+
+		//when & then
+		assertThatThrownBy(() -> answerService.chooseAnswer(answer.getId(), notQuestioner))
+			.isInstanceOf(ValidationException.class)
+			.hasMessageContaining(QuestionPostErrorCode.NOT_AUTHORIZED.getMessage());
 	}
 }
