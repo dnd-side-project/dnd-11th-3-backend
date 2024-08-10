4866b23fc772632a4362cd13e2064a2a7942b112
dudxo <father8475@naver.com>
2024-08-10 12:39:30 +0900

    [feat] : 로그아웃 에러코드 추가

--- src/main/java/com/dnd/gongmuin/member/exception/MemberErrorCode.java
+++ src/main/java/com/dnd/gongmuin/member/exception/MemberErrorCode.java
@@ -10,7 +10,8 @@ import lombok.RequiredArgsConstructor;
 public enum MemberErrorCode implements ErrorCode {
 
 	NOT_FOUND_MEMBER("특정 회원을 찾을 수 없습니다.", "MEMBER_001"),
-	NOT_FOUND_NEW_MEMBER("신규 회원이 아닙니다.", "MEMBER_002");
+	NOT_FOUND_NEW_MEMBER("신규 회원이 아닙니다.", "MEMBER_002"),
+	FAIL_LOGOUT("로그아웃을 실패했습니다.", "MEMBER_003");
 
 	private final String message;
 	private final String code;
