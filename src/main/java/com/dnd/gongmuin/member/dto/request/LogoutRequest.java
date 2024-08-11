7e22abf5696ea3590872f722952616d151ee1a16
dudxo <father8475@naver.com>
2024-08-11 14:35:27 +0900

    [feat] : 로그아웃/토큰 재발급 요청 DTO Valid Message 수정

--- src/main/java/com/dnd/gongmuin/member/dto/request/LogoutRequest.java
+++ src/main/java/com/dnd/gongmuin/member/dto/request/LogoutRequest.java
@@ -3,7 +3,7 @@ package com.dnd.gongmuin.member.dto.request;
 import jakarta.validation.constraints.NotEmpty;
 
 public record LogoutRequest(
-	@NotEmpty(message = "잘못된 요청입니다.")
+	@NotEmpty(message = "AccessToken을 입력해주세요.")
 	String accessToken
 ) {
 
