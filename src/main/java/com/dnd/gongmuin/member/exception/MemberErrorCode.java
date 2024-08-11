ff4d80d2cb3dd0f46a1c01d2f20b88e5af60e22d
dudxo <father8475@naver.com>
2024-08-11 14:36:23 +0900

    [feat] : 로그아웃 에러 코드 message 수정

--- src/main/java/com/dnd/gongmuin/member/exception/MemberErrorCode.java
+++ src/main/java/com/dnd/gongmuin/member/exception/MemberErrorCode.java
@@ -11,7 +11,7 @@ public enum MemberErrorCode implements ErrorCode {
 
 	NOT_FOUND_MEMBER("특정 회원을 찾을 수 없습니다.", "MEMBER_001"),
 	NOT_FOUND_NEW_MEMBER("신규 회원이 아닙니다.", "MEMBER_002"),
-	FAIL_LOGOUT("로그아웃을 실패했습니다.", "MEMBER_003"),
+	LOGOUT_FAILED("로그아웃을 실패했습니다.", "MEMBER_003"),
 	NOT_ENOUGH_CREDIT("보유한 크레딧이 부족합니다.", "MEMBER_004");
 
 	private final String message;
