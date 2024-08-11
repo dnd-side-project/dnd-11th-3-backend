4d59ff05ca361782bdf3e6b588488455f482dab2
hs12 <loveanna1228@gmail.com>
2024-08-11 13:38:10 +0900

    [feat] : 질문글 검색 요청 DTO 추가

---
+++ src/main/java/com/dnd/gongmuin/question_post/dto/request/QuestionPostSearchCondition.java
@@ -0,0 +1,13 @@
+package com.dnd.gongmuin.question_post.dto.request;
+
+import java.util.List;
+
+import jakarta.validation.constraints.NotBlank;
+
+public record QuestionPostSearchCondition (
+	@NotBlank(message = "검색어를 입력해주세요.")
+	String keyword,
+	List<String> jobGroups,
+	boolean isChosen
+){
+}
