85f8703192e38464e133c5dcd9753928e3601757
hs12 <loveanna1228@gmail.com>
2024-08-11 17:54:33 +0900

    [feat] : JobGroup 필드 추가 및 변환함수 추가

--- src/main/java/com/dnd/gongmuin/member/domain/JobGroup.java
+++ src/main/java/com/dnd/gongmuin/member/domain/JobGroup.java
@@ -1,6 +1,7 @@
 package com.dnd.gongmuin.member.domain;
 
 import java.util.Arrays;
+import java.util.List;
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
@@ -9,7 +10,9 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum JobGroup {
 
-	ENGINEERING("공업"); // TODO: 7/20/24 필드 추가
+	ENGINEERING("공업"),
+	ADMINISTRATION("행정"),
+	MACHINE("기계");
 
 	private final String label;
 
@@ -18,6 +21,12 @@ public enum JobGroup {
 			.filter(group -> group.isEqual(input))
 			.findAny()
 			.orElseThrow(IllegalArgumentException::new);
+	}
+
+	public static List<JobGroup> of(List<String> labels){
+		return labels.stream()
+			.map(JobGroup::of)
+			.toList();
 	}
 
 	private boolean isEqual(String input) {
