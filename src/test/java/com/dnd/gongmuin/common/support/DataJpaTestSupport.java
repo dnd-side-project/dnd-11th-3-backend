15675a1901c56d929ccf3850322b301dd7ce761a
hs12 <loveanna1228@gmail.com>
2024-08-11 19:13:29 +0900

    [test] : repository test 추상 클래스에 querydsl 설정 파일 포함

--- src/test/java/com/dnd/gongmuin/common/support/DataJpaTestSupport.java
+++ src/test/java/com/dnd/gongmuin/common/support/DataJpaTestSupport.java
@@ -2,13 +2,16 @@ package com.dnd.gongmuin.common.support;
 
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Import;
+import org.springframework.stereotype.Repository;
 
+import com.dnd.gongmuin.common.config.QueryDslConfig;
 import com.dnd.gongmuin.config.TestAuditingConfig;
 
 //repository용
-@DataJpaTest
+@DataJpaTest(includeFilters = @ComponentScan.Filter(Repository.class))
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
-@Import(TestAuditingConfig.class)
+@Import({TestAuditingConfig.class, QueryDslConfig.class})
 public abstract class DataJpaTestSupport extends TestContainerSupport {
 }
