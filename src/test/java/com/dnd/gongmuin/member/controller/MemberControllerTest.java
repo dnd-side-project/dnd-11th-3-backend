c69440b45668e4370a8f8a1061d97910e237582e
dudxo <father8475@naver.com>
2024-08-11 16:41:36 +0900

    [test] : 회원가입 API 통합 테스트를 위한 신규 회원 저장 로직 추가

--- src/test/java/com/dnd/gongmuin/member/controller/MemberControllerTest.java
+++ src/test/java/com/dnd/gongmuin/member/controller/MemberControllerTest.java
@@ -5,6 +5,8 @@ import static org.springframework.http.MediaType.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+import java.util.Date;
+
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -19,6 +21,9 @@ import com.dnd.gongmuin.member.dto.request.LogoutRequest;
 import com.dnd.gongmuin.member.dto.request.ReissueRequest;
 import com.dnd.gongmuin.member.dto.request.ValidateNickNameRequest;
 import com.dnd.gongmuin.member.repository.MemberRepository;
+import com.dnd.gongmuin.security.jwt.util.TokenProvider;
+import com.dnd.gongmuin.security.oauth2.AuthInfo;
+import com.dnd.gongmuin.security.oauth2.CustomOauth2User;
 
 @DisplayName("[MemberController] 통합테스트")
 class MemberControllerTest extends ApiTestSupport {
@@ -28,6 +33,9 @@ class MemberControllerTest extends ApiTestSupport {
 
 	@Autowired
 	private RedisTemplate<String, Object> redisTemplate;
+
+	@Autowired
+	private TokenProvider tokenProvider;
 
 	@AfterEach
 	void tearDown() {
@@ -58,6 +66,12 @@ class MemberControllerTest extends ApiTestSupport {
 	void signUp() throws Exception {
 		// given
 		AdditionalInfoRequest request = new AdditionalInfoRequest("dsaf@korea.kr", "회원", "공업", "가스");
+
+		Member savedMember = memberRepository.save(MemberFixture.member3());
+		AuthInfo authInfo = AuthInfo.of(savedMember.getSocialName(), savedMember.getSocialEmail());
+		String token = tokenProvider.generateAccessToken(new CustomOauth2User(authInfo), new Date());
+		this.loginMember = savedMember;
+		this.accessToken = "Bearer " + token;
 
 		// when  // then
 		mockMvc.perform(post("/api/auth/member")
