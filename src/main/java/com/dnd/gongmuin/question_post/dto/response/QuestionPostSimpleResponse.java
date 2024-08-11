0e9d34c3f56d65388b157a903255e3653633323c
hs12 <loveanna1228@gmail.com>
2024-08-11 13:38:02 +0900

    [feat] : 질문글 검색 DTO 추가

---
+++ src/main/java/com/dnd/gongmuin/question_post/dto/response/QuestionPostSimpleResponse.java
@@ -0,0 +1,12 @@
+package com.dnd.gongmuin.question_post.dto.response;
+
+public record QuestionPostSimpleResponse(
+	Long questionPostId,
+	String title,
+	String content,
+	String jobGroup,
+	int credit,
+	String createdAt
+	// TODO: 8/11/24 북마크 수, 추천수 추가
+){
+}
