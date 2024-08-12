a90ccd27f95f2da91e533df771e42ec2155ced30
hs12 <loveanna1228@gmail.com>
2024-08-12 16:02:20 +0900

    [feat] : 질문글 검색 API 로직 추가

--- src/main/java/com/dnd/gongmuin/question_post/controller/QuestionPostController.java
+++ src/main/java/com/dnd/gongmuin/question_post/controller/QuestionPostController.java
@@ -1,5 +1,6 @@
 package com.dnd.gongmuin.question_post.controller;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,9 +10,12 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.dnd.gongmuin.common.dto.PageResponse;
 import com.dnd.gongmuin.member.domain.Member;
-import com.dnd.gongmuin.question_post.dto.response.QuestionPostDetailResponse;
+import com.dnd.gongmuin.question_post.dto.request.QuestionPostSearchCondition;
 import com.dnd.gongmuin.question_post.dto.request.RegisterQuestionPostRequest;
+import com.dnd.gongmuin.question_post.dto.response.QuestionPostDetailResponse;
+import com.dnd.gongmuin.question_post.dto.response.QuestionPostSimpleResponse;
 import com.dnd.gongmuin.question_post.service.QuestionPostService;
 
 import io.swagger.v3.oas.annotations.Operation;
@@ -46,6 +50,18 @@ public class QuestionPostController {
 		@PathVariable("questionPostId") Long questionPostId
 	) {
 		QuestionPostDetailResponse response = questionPostService.getQuestionPostById(questionPostId);
+		return ResponseEntity.ok(response);
+	}
+
+	@Operation(summary = "질문글 검색 API", description = "질문글을 키워드로 검색하고 정렬, 필터링을 한다.")
+	@ApiResponse(useReturnTypeSchema = true)
+	@PostMapping("/search")
+	public ResponseEntity<PageResponse<QuestionPostSimpleResponse>> searchQuestionPost(
+		@Valid @RequestBody QuestionPostSearchCondition condition,
+		Pageable pageable
+	) {
+		PageResponse<QuestionPostSimpleResponse> response = questionPostService.searchQuestionPost(
+			condition, pageable);
 		return ResponseEntity.ok(response);
 	}
 }
