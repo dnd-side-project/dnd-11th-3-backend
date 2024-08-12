a856ffd33511e0fae469ca45959dde731815fdd5
dudxo <father8475@naver.com>
2024-08-12 22:43:18 +0900

    [feat] : Security ErrorCode 추가

---
+++ src/main/java/com/dnd/gongmuin/security/exception/SecurityErrorCode.java
@@ -0,0 +1,15 @@
+package com.dnd.gongmuin.security.exception;
+
+import com.dnd.gongmuin.common.exception.ErrorCode;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum SecurityErrorCode implements ErrorCode {
+	UNAUTHORIZED("비인가 사용자 요청입니다.", "SECURITY_001");
+
+	private final String message;
+	private final String code;
+}
