0f426b0069151c3ede4cb29776a350f9fd713357
dudxo <father8475@naver.com>
2024-08-13 18:45:12 +0900

    [feat] : 공무원 이메일이 일치하는 회원 반환 쿼리 메서드 추가

--- src/main/java/com/dnd/gongmuin/member/repository/MemberRepository.java
+++ src/main/java/com/dnd/gongmuin/member/repository/MemberRepository.java
@@ -14,4 +14,6 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
 	boolean existsByOfficialEmail(String officialEmail);
 
 	boolean existsBySocialEmail(String socialEmail);
+
+	Member findByOfficialEmail(String officialEmail);
 }
