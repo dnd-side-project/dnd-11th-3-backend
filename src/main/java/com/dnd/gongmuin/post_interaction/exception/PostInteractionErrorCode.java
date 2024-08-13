ba1c5ec21ac9c551f90003d8fb79c2d523a68aff
hs12 <loveanna1228@gmail.com>
2024-08-13 22:14:12 +0900

    [feat] : 상태변경 에러코드 수정

--- src/main/java/com/dnd/gongmuin/post_interaction/exception/PostInteractionErrorCode.java
+++ src/main/java/com/dnd/gongmuin/post_interaction/exception/PostInteractionErrorCode.java
@@ -9,7 +9,7 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum PostInteractionErrorCode implements ErrorCode {
 
-	NOT_FOUND_POST_INTERACTION("질문 게시글이 존재하지 않습니다.", "PI_001"),
+	NOT_FOUND_POST_INTERACTION("상호작용 이력이 존재하지 않습니다.", "PI_001"),
 	ALREADY_INTERACTED("이미 상호작용한 게시글입니다.", "PI_002"),
 	ALREADY_UNINTERACTED("이미 상호작용 취소한 게시글입니다.", "PI_003"),
 	INTERACTION_NOT_ALLOWED("본인 게시물은 상호작용할 수 없습니다", "PI_004");
