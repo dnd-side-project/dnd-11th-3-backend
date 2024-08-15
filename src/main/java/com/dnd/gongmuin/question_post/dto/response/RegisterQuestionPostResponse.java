4d9a06dd64adcadef579df38db50cceeb95a0705
hs12 <loveanna1228@gmail.com>
2024-08-15 21:07:47 +0900

    [feat] : 질문글 등록 응답 dto 생성

---
+++ src/main/java/com/dnd/gongmuin/question_post/dto/response/RegisterQuestionPostResponse.java
@@ -0,0 +1,15 @@
+package com.dnd.gongmuin.question_post.dto.response;
+
+import java.util.List;
+
+public record RegisterQuestionPostResponse (
+	Long questionPostId,
+	String title,
+	String content,
+	List<String> imageUrls,
+	int reward,
+	String targetJobGroup,
+	MemberInfo memberInfo,
+	String createdAt
+){
+}
