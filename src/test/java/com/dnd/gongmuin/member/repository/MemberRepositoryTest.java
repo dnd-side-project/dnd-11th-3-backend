bf0688cdd0f670026c69306b4d9ab9a52aab2a2f
dudxo <father8475@naver.com>
2024-08-16 01:26:50 +0900

    [test] : 특정 회원 답변 작성 질문 목록 리스트 조회 테스트

--- src/test/java/com/dnd/gongmuin/member/repository/MemberRepositoryTest.java
+++ src/test/java/com/dnd/gongmuin/member/repository/MemberRepositoryTest.java
@@ -13,10 +13,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Slice;
 
+import com.dnd.gongmuin.answer.domain.Answer;
+import com.dnd.gongmuin.answer.repository.AnswerRepository;
+import com.dnd.gongmuin.common.fixture.AnswerFixture;
 import com.dnd.gongmuin.common.fixture.MemberFixture;
 import com.dnd.gongmuin.common.fixture.QuestionPostFixture;
 import com.dnd.gongmuin.common.support.DataJpaTestSupport;
 import com.dnd.gongmuin.member.domain.Member;
+import com.dnd.gongmuin.member.dto.response.AnsweredQuestionPostsByMemberResponse;
 import com.dnd.gongmuin.member.dto.response.QuestionPostsByMemberResponse;
 import com.dnd.gongmuin.question_post.domain.QuestionPost;
 import com.dnd.gongmuin.question_post.repository.QuestionPostRepository;
@@ -30,6 +34,9 @@ class MemberRepositoryTest extends DataJpaTestSupport {
 
 	@Autowired
 	QuestionPostRepository questionPostRepository;
+
+	@Autowired
+	AnswerRepository answerRepository;
 
 	@DisplayName("소셜이메일로 특정 회원을 조회한다.")
 	@Test
@@ -45,7 +52,7 @@ class MemberRepositoryTest extends DataJpaTestSupport {
 		assertThat(findMember.getNickname()).isEqualTo("공무인1");
 	}
 
-	@DisplayName("자신 작성한 게시글 목록만 조회할 수 있다.")
+	@DisplayName("자신이 작성한 게시글 목록만 조회할 수 있다.")
 	@Test
 	void getQuestionPostsByMember() {
 		// given
@@ -73,6 +80,50 @@ class MemberRepositoryTest extends DataJpaTestSupport {
 			() -> assertThat(postsByMember).extracting(QuestionPostsByMemberResponse::questionPostId)
 				.containsExactly(
 					2L,
+					1L
+				)
+		);
+	}
+
+	@DisplayName("자신이 댓글 단 게시글 목록만 조회할 수 있다.")
+	@Test
+	void getAnsweredQuestionPostsByMember() {
+		// given
+		Member member1 = MemberFixture.member();
+		Member member2 = MemberFixture.member2();
+		memberRepository.saveAll(List.of(member1, member2));
+
+		QuestionPost questionPost1 = QuestionPostFixture.questionPost(member1, "첫 번째 게시글입니다.");
+		QuestionPost questionPost2 = QuestionPostFixture.questionPost(member1, "두 번째 게시글입니다.");
+		QuestionPost questionPost3 = QuestionPostFixture.questionPost(member2, "세 번째 게시글입니다.");
+		questionPostRepository.saveAll(List.of(questionPost1, questionPost2, questionPost3));
+
+		Answer answer1 = AnswerFixture.answer(questionPost1.getId(), member1);
+		Answer answer2 = AnswerFixture.answer(questionPost1.getId(), member2);
+		Answer answer3 = AnswerFixture.answer(questionPost2.getId(), member2);
+		Answer answer4 = AnswerFixture.answer(questionPost3.getId(), member1);
+		answerRepository.saveAll(List.of(answer1, answer2, answer3, answer4));
+
+		// when
+		Slice<AnsweredQuestionPostsByMemberResponse> postsByMember =
+			memberRepository.getAnsweredQuestionPostsByMember(member1, pageRequest);
+
+		// then
+		Assertions.assertAll(
+			() -> assertThat(postsByMember).hasSize(2),
+			() -> assertThat(postsByMember).extracting(AnsweredQuestionPostsByMemberResponse::title)
+				.containsExactly(
+					"세 번째 게시글입니다.",
+					"첫 번째 게시글입니다."
+				),
+			() -> assertThat(postsByMember).extracting(AnsweredQuestionPostsByMemberResponse::questionPostId)
+				.containsExactly(
+					3L,
+					1L
+				),
+			() -> assertThat(postsByMember).extracting(AnsweredQuestionPostsByMemberResponse::answerId)
+				.containsExactly(
+					4L,
 					1L
 				)
 		);
