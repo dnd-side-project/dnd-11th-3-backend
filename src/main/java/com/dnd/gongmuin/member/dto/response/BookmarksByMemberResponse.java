10fd9680b31247fa360ebe596b8a75b2c33c0f5f
dudxo <father8475@naver.com>
2024-08-17 01:48:56 +0900

    [feat] : 스크랩 전체 조회 응답 DTO 추가

---
+++ src/main/java/com/dnd/gongmuin/member/dto/response/BookmarksByMemberResponse.java
@@ -0,0 +1,40 @@
+package com.dnd.gongmuin.member.dto.response;
+
+import com.dnd.gongmuin.post_interaction.domain.InteractionCount;
+import com.dnd.gongmuin.question_post.domain.QuestionPost;
+import com.querydsl.core.annotations.QueryProjection;
+
+public record BookmarksByMemberResponse(
+	Long questionPostId,
+	String title,
+	String content,
+	String jobGroup,
+	int reward,
+	String updatedAt,
+	boolean isChosen,
+	int savedTotalCount,
+	int recommendTotalCount
+) {
+	@QueryProjection
+	public BookmarksByMemberResponse(
+		QuestionPost questionPost,
+		InteractionCount savedCount,
+		InteractionCount recommendCount
+	) {
+		this(
+			questionPost.getId(),
+			questionPost.getTitle(),
+			questionPost.getContent(),
+			questionPost.getJobGroup().getLabel(),
+			questionPost.getReward(),
+			questionPost.getUpdatedAt().toString(),
+			questionPost.getIsChosen(),
+			extractTotalCount(savedCount),
+			extractTotalCount(recommendCount)
+		);
+	}
+
+	private static int extractTotalCount(InteractionCount interactionCount) {
+		return interactionCount != null ? interactionCount.getCount() : 0;
+	}
+}
