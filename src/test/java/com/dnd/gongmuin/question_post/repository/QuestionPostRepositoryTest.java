a982fe56b91a7777c8f14201f8de05bf4a3acee9
hs12 <loveanna1228@gmail.com>
2024-08-17 15:48:53 +0900

    [test] : 엔티티가 아닌 DTO로 조회하도록 쿼리 수정 테스트

--- src/test/java/com/dnd/gongmuin/question_post/repository/QuestionPostRepositoryTest.java
+++ src/test/java/com/dnd/gongmuin/question_post/repository/QuestionPostRepositoryTest.java
@@ -12,14 +12,25 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.test.util.ReflectionTestUtils;
 
+import com.dnd.gongmuin.common.fixture.InteractionCountFixture;
+import com.dnd.gongmuin.common.fixture.InteractionFixture;
 import com.dnd.gongmuin.common.fixture.MemberFixture;
 import com.dnd.gongmuin.common.fixture.QuestionPostFixture;
 import com.dnd.gongmuin.common.support.DataJpaTestSupport;
 import com.dnd.gongmuin.member.domain.Member;
 import com.dnd.gongmuin.member.repository.MemberRepository;
+import com.dnd.gongmuin.post_interaction.domain.Interaction;
+import com.dnd.gongmuin.post_interaction.domain.InteractionCount;
+import com.dnd.gongmuin.post_interaction.domain.InteractionType;
+import com.dnd.gongmuin.post_interaction.repository.InteractionCountRepository;
+import com.dnd.gongmuin.post_interaction.repository.InteractionRepository;
 import com.dnd.gongmuin.question_post.domain.QuestionPost;
 import com.dnd.gongmuin.question_post.dto.request.QuestionPostSearchCondition;
+import com.dnd.gongmuin.question_post.dto.response.QuestionPostSimpleResponse;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @DisplayName("[질문글 동적 쿼리 테스트]")
 class QuestionPostRepositoryTest extends DataJpaTestSupport {
 
@@ -32,12 +43,18 @@ class QuestionPostRepositoryTest extends DataJpaTestSupport {
 	@Autowired
 	private QuestionPostRepository questionPostRepository;
 
+	@Autowired
+	private InteractionRepository interactionRepository;
+
+	@Autowired
+	private InteractionCountRepository interactionCountRepository;
+
 	@BeforeEach
 	void setup() {
 		member = memberRepository.save(MemberFixture.member());
 	}
 
-	@DisplayName("검색어로 필터링할 수 있다")
+	@DisplayName("검색어로 필터링할 수 있다.")
 	@Test
 	void question_post_search_filter() {
 		//given
@@ -53,18 +70,19 @@ class QuestionPostRepositoryTest extends DataJpaTestSupport {
 		);
 
 		//when
-		List<QuestionPost> questionPosts = questionPostRepository
+		List<QuestionPostSimpleResponse> responses = questionPostRepository
 			.searchQuestionPosts(condition, pageRequest)
 			.getContent();
 
 		//then
 		Assertions.assertAll(
-			() -> assertThat(questionPosts).hasSize(2),
-			() -> assertThat(questionPosts).containsExactly(questionPost1, questionPost2)
+			() -> assertThat(responses).hasSize(2),
+			() -> assertThat(responses.get(0).questionPostId()).isEqualTo(questionPost1.getId()),
+			() -> assertThat(responses.get(1).questionPostId()).isEqualTo(questionPost2.getId())
 		);
 	}
 
-	@DisplayName("[직군으로 질문글을 필터링할 수 있다.]")
+	@DisplayName("직군으로 질문글을 필터링할 수 있다.")
 	@Test
 	void question_post_jobgroup_filter() {
 		//given
@@ -81,18 +99,19 @@ class QuestionPostRepositoryTest extends DataJpaTestSupport {
 		);
 
 		//when
-		List<QuestionPost> questionPosts = questionPostRepository
+		List<QuestionPostSimpleResponse> responses = questionPostRepository
 			.searchQuestionPosts(condition, pageRequest)
 			.getContent();
 
 		//then
 		Assertions.assertAll(
-			() -> assertThat(questionPosts).hasSize(2),
-			() -> assertThat(questionPosts).containsExactly(questionPost1, questionPost2)
+			() -> assertThat(responses).hasSize(2),
+			() -> assertThat(responses.get(0).questionPostId()).isEqualTo(questionPost1.getId()),
+			() -> assertThat(responses.get(1).questionPostId()).isEqualTo(questionPost2.getId())
 		);
 	}
 
-	@DisplayName("[채택 여부로 질문글을 필터링할 수 있다.]")
+	@DisplayName("채택 여부로 질문글을 필터링할 수 있다.")
 	@Test
 	void question_post_ischosen_filter() {
 		//given
@@ -109,15 +128,54 @@ class QuestionPostRepositoryTest extends DataJpaTestSupport {
 		);
 
 		//when
-		List<QuestionPost> questionPosts = questionPostRepository
+		List<QuestionPostSimpleResponse> responses = questionPostRepository
 			.searchQuestionPosts(condition, pageRequest)
 			.getContent();
 
 		//then
 		Assertions.assertAll(
-			() -> assertThat(questionPosts).hasSize(1),
-			() -> assertThat(questionPosts).containsExactly(questionPost1)
+			() -> assertThat(responses).hasSize(1),
+			() -> assertThat(responses.get(0).questionPostId()).isEqualTo(questionPost1.getId())
 		);
+	}
+
+	@DisplayName("추천수, 저장수를 함께 조회할 수 있다.")
+	@Test
+	void question_post_join_interactionCount() {
+		//given
+		QuestionPost questionPost = QuestionPostFixture.questionPost(member);
+		questionPostRepository.save(questionPost);
+
+		QuestionPostSearchCondition condition = new QuestionPostSearchCondition(
+			null,
+			null,
+			null
+		);
+
+		interactPost(questionPost.getId(), InteractionType.SAVED);
+		interactPost(questionPost.getId(), InteractionType.RECOMMEND);
+
+		//when
+		List<QuestionPostSimpleResponse> responses = questionPostRepository
+			.searchQuestionPosts(condition, pageRequest)
+			.getContent();
+		System.out.println(responses);
+		//then
+		Assertions.assertAll(
+			() -> assertThat(responses).hasSize(1),
+			() -> assertThat(responses.get(0).questionPostId()).isEqualTo(questionPost.getId()),
+			() -> assertThat(responses.get(0).savedCount()).isEqualTo(1),
+			() -> assertThat(responses.get(0).recommendCount()).isEqualTo(1)
+		);
+	}
+
+	private void interactPost(Long questionPostId, InteractionType type){
+		Interaction interaction =
+			InteractionFixture.interaction(type, 2L, questionPostId);
+		interactionRepository.save(interaction);
+		InteractionCount interactionCount =
+			InteractionCountFixture.interactionCount(type, questionPostId);
+		interactionCountRepository.save(interactionCount);
 	}
 
 }
