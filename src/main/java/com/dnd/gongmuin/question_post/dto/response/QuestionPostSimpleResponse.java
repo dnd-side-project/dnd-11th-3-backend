23101bfe6d50659339344f9441cbbfb98d80d954
hs12 <loveanna1228@gmail.com>
2024-08-17 13:43:14 +0900

    [feat] : querydsl용 dto 생성

--- src/main/java/com/dnd/gongmuin/question_post/dto/response/QuestionPostSimpleResponse.java
+++ src/main/java/com/dnd/gongmuin/question_post/dto/response/QuestionPostSimpleResponse.java
@@ -1,5 +1,9 @@
 package com.dnd.gongmuin.question_post.dto.response;
 
+import com.dnd.gongmuin.post_interaction.domain.InteractionCount;
+import com.dnd.gongmuin.question_post.domain.QuestionPost;
+import com.querydsl.core.annotations.QueryProjection;
+
 public record QuestionPostSimpleResponse(
 	Long questionPostId,
 	String title,
@@ -7,7 +11,31 @@ public record QuestionPostSimpleResponse(
 	String jobGroup,
 	int reward,
 	String createdAt,
-	boolean isChosen
-	// TODO: 8/11/24 북마크 수, 추천수 추가
+	boolean isChosen,
+	int savedCount,
+	int recommendCount
 ) {
+
+	@QueryProjection
+	public QuestionPostSimpleResponse(
+		QuestionPost questionPost,
+		InteractionCount savedCount,
+		InteractionCount recommendCount
+	) {
+		this(
+			questionPost.getId(),
+			questionPost.getTitle(),
+			questionPost.getContent(),
+			questionPost.getJobGroup().getLabel(),
+			questionPost.getReward(),
+			questionPost.getCreatedAt().toString(),
+			questionPost.getIsChosen(),
+			getCount(savedCount),
+			getCount(recommendCount)
+		);
+	}
+
+	private static int getCount(InteractionCount interactionCount) {
+		return interactionCount != null ? interactionCount.getCount() : 0;
+	}
 }
