ae380bdd404d130e432b17dc22ed97daa8eb89b7
hs12 <loveanna1228@gmail.com>
2024-08-17 15:56:28 +0900

    [chore] : 상호작용 API 스웨거 명세 추가

--- src/main/java/com/dnd/gongmuin/post_interaction/controller/InteractionController.java
+++ src/main/java/com/dnd/gongmuin/post_interaction/controller/InteractionController.java
@@ -12,14 +12,18 @@ import com.dnd.gongmuin.post_interaction.domain.InteractionType;
 import com.dnd.gongmuin.post_interaction.dto.InteractionResponse;
 import com.dnd.gongmuin.post_interaction.service.InteractionService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 
+@Tag(name = "상호작용 API")
 @RestController
 @RequiredArgsConstructor
 public class InteractionController {
 
 	private final InteractionService interactionService;
 
+	@Operation(summary = "상호작용 등록 API", description = "게시글을 추천하거나 북마크한다.")
 	@PostMapping("/api/question-posts/{questionPostId}/activated")
 	public ResponseEntity<InteractionResponse> activateInteraction(
 		@PathVariable("questionPostId") Long questionPostId,
@@ -34,6 +38,7 @@ public class InteractionController {
 		return ResponseEntity.ok(response);
 	}
 
+	@Operation(summary = "상호작용 등록 API", description = "게시글을 추천하거나 북마크 취소한다.")
 	@PostMapping("/api/question-posts/{questionPostId}/inactivated")
 	public ResponseEntity<InteractionResponse> inactivateInteraction(
 		@PathVariable("questionPostId") Long questionPostId,
