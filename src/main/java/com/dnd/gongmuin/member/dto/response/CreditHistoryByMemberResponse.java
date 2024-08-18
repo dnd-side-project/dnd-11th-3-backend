6da2a00387cbab959817ea0ab625b67dfa92c0c1
dudxo <father8475@naver.com>
2024-08-19 00:33:28 +0900

    [feat] : 크레딧 내역 목록 응답 DTO 추가

---
+++ src/main/java/com/dnd/gongmuin/member/dto/response/CreditHistoryByMemberResponse.java
@@ -0,0 +1,23 @@
+package com.dnd.gongmuin.member.dto.response;
+
+import com.dnd.gongmuin.credit_history.domain.CreditHistory;
+import com.querydsl.core.annotations.QueryProjection;
+
+public record CreditHistoryByMemberResponse(
+	Long id,
+	String typeMessage,
+	String type,
+	String detail,
+	int amount
+) {
+	@QueryProjection
+	public CreditHistoryByMemberResponse(CreditHistory creditHistory) {
+		this(
+			creditHistory.getId(),
+			creditHistory.getType().getLabel(),
+			creditHistory.getType().getDetail(),
+			creditHistory.getDetail(),
+			creditHistory.getAmount()
+		);
+	}
+}
