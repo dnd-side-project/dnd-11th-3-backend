ed06f7300c95d06e9145f19c37cc54fa9cccf557
hs12 <loveanna1228@gmail.com>
2024-08-18 10:25:20 +0900

    [test] : 추천 게시물 조회 controller 로직 테스트

--- src/test/java/com/dnd/gongmuin/question_post/controller/QuestionPostControllerTest.java
+++ src/test/java/com/dnd/gongmuin/question_post/controller/QuestionPostControllerTest.java
@@ -14,9 +14,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.util.ReflectionTestUtils;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 
+import com.dnd.gongmuin.common.fixture.InteractionCountFixture;
+import com.dnd.gongmuin.common.fixture.InteractionFixture;
 import com.dnd.gongmuin.common.fixture.QuestionPostFixture;
 import com.dnd.gongmuin.common.support.ApiTestSupport;
 import com.dnd.gongmuin.member.exception.MemberErrorCode;
+import com.dnd.gongmuin.post_interaction.domain.Interaction;
+import com.dnd.gongmuin.post_interaction.domain.InteractionCount;
+import com.dnd.gongmuin.post_interaction.domain.InteractionType;
+import com.dnd.gongmuin.post_interaction.repository.InteractionCountRepository;
+import com.dnd.gongmuin.post_interaction.repository.InteractionRepository;
 import com.dnd.gongmuin.question_post.domain.QuestionPost;
 import com.dnd.gongmuin.question_post.dto.request.RegisterQuestionPostRequest;
 import com.dnd.gongmuin.question_post.repository.QuestionPostRepository;
@@ -27,10 +34,18 @@ class QuestionPostControllerTest extends ApiTestSupport {
 	@Autowired
 	private QuestionPostRepository questionPostRepository;
 
+	@Autowired
+	private InteractionRepository interactionRepository;
+
+	@Autowired
+	private InteractionCountRepository interactionCountRepository;
+
 	@AfterEach
 	void teardown() {
 		memberRepository.deleteAll();
 		questionPostRepository.deleteAll();
+		interactionRepository.deleteAll();
+		interactionCountRepository.deleteAll();
 	}
 
 	@DisplayName("[질문글을 등록할 수 있다.]")
@@ -173,5 +188,34 @@ class QuestionPostControllerTest extends ApiTestSupport {
 			.andDo(MockMvcResultHandlers.print())
 			.andExpect(jsonPath("$.size").value(1))
 			.andExpect(jsonPath("$.content[0].questionPostId").value(questionPost2.getId()));
+	}
+
+	@DisplayName("[직군에 맞는 추천 질문 게시물을 조회할 수 있다. 추천순>북마크순]")
+	@Test
+	void getRecommendQuestionPosts() throws Exception {
+		QuestionPost questionPost1 = questionPostRepository.save(QuestionPostFixture.questionPost(loginMember));
+		QuestionPost questionPost2 = questionPostRepository.save(QuestionPostFixture.questionPost(loginMember));
+		QuestionPost questionPost3 = questionPostRepository.save(QuestionPostFixture.questionPost(loginMember));
+		questionPostRepository.saveAll(List.of(questionPost1, questionPost2, questionPost3));
+
+		interactPost(questionPost3.getId(), InteractionType.RECOMMEND);
+		interactPost(questionPost1.getId(), InteractionType.SAVED);
+		mockMvc.perform(get("/api/question-posts/recommends")
+				.header(AUTHORIZATION, accessToken))
+			.andExpect(status().isOk())
+			.andDo(MockMvcResultHandlers.print())
+			.andExpect(jsonPath("$.size").value(3))
+			.andExpect(jsonPath("$.content[0].questionPostId").value(questionPost3.getId()))
+			.andExpect(jsonPath("$.content[1].questionPostId").value(questionPost1.getId()))
+			.andExpect(jsonPath("$.content[2].questionPostId").value(questionPost2.getId()));
+	}
+
+	private void interactPost(Long questionPostId, InteractionType type) {
+		Interaction interaction =
+			InteractionFixture.interaction(type, 2L, questionPostId);
+		interactionRepository.save(interaction);
+		InteractionCount interactionCount =
+			InteractionCountFixture.interactionCount(type, questionPostId);
+		interactionCountRepository.save(interactionCount);
 	}
 }
