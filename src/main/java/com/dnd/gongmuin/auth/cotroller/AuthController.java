c1694d9b274529fe4d9d209055e84096e5b89e60
dudxo <father8475@naver.com>
2024-08-19 15:50:47 +0900

    [fix] : 카카오 로그인 flow 변경에 따른 api 삭제

--- src/main/java/com/dnd/gongmuin/auth/cotroller/AuthController.java
+++ src/main/java/com/dnd/gongmuin/auth/cotroller/AuthController.java
@@ -1,12 +1,7 @@
 package com.dnd.gongmuin.auth.cotroller;
 
-import java.net.URI;
-
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -40,17 +35,7 @@ public class AuthController {
 	private final AuthService authService;
 	private final MemberService memberService;
 
-	@Operation(summary = "카카오 로그인 API", description = "카카오 로그인 페이지로 이동 요청한다.(사용불가!!)")
-	@ApiResponse(useReturnTypeSchema = true)
-	@GetMapping("/signin/kakao")
-	public ResponseEntity<?> kakaoLoginRedirect() {
-		HttpHeaders httpHeaders = new HttpHeaders();
-		// 카카오 로그인 페이지로 리다이렉트
-		httpHeaders.setLocation(URI.create("/oauth2/authorization/kakao"));
-		return new ResponseEntity<>(httpHeaders, HttpStatus.MOVED_PERMANENTLY);
-	}
-
-	@Operation(summary = "임시 로그인/회원가입(토큰 발급) API", description = "로그인 또는 회원가입 후 토큰을 발급한다.")
+	@Operation(summary = "임시 회원가입(토큰 발급) API", description = "로그인 또는 회원가입 후 토큰을 발급한다.")
 	@ApiResponse(useReturnTypeSchema = true)
 	@PostMapping("/token")
 	public ResponseEntity<String> getTempToken(@RequestBody @Valid TempLoginRequest request) {
