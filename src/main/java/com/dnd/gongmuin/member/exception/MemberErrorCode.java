51e33ea9fcb7e5493f2e61cfb281a71ab5547517
Son Gahyun <77109954+hyun2371@users.noreply.github.com>
2024-08-20 19:53:42 +0900

    [feat #61] String-> Enum 변환 실패 시 커스텀 예외 반환 (#62)
    * [refactor] : 에러 코드 메시지 수정
    
    * [feat] : 회원 에러코드에 직군, 직렬 관련 에러 코드 추가
    
    * [feat] : String->enum 값 변환 실패 시 커스텀 예외 던지기
    
    * [refactor] : 클래스 레벨 requestMapping 제거
    
    * [chore] : Swagger 명세 수정
    
    * [feat] : 상호작용 타입 변환 실패 시 커스텀 에러 던지도록 수정

--- src/main/java/com/dnd/gongmuin/member/exception/MemberErrorCode.java
+++ src/main/java/com/dnd/gongmuin/member/exception/MemberErrorCode.java
@@ -14,7 +14,9 @@ public enum MemberErrorCode implements ErrorCode {
 	LOGOUT_FAILED("로그아웃을 실패했습니다.", "MEMBER_003"),
 	NOT_ENOUGH_CREDIT("보유한 크레딧이 부족합니다.", "MEMBER_004"),
 	UPDATE_PROFILE_FAILED("프로필 수정에 실패했습니다.", "MEMBER_005"),
-	QUESTION_POSTS_BY_MEMBER_FAILED("마이페이지 게시글 목록을 불러오는데 실패했습니다", "MEMBER_006");
+	QUESTION_POSTS_BY_MEMBER_FAILED("마이페이지 게시글 목록을 불러오는데 실패했습니다", "MEMBER_006"),
+	NOT_FOUND_JOB_GROUP("직군을 올바르게 입력해주세요.", "MEMBER_007"),
+	NOT_FOUND_JOB_CATEGORY("직렬을 올바르게 입력해주세요.", "MEMBER_008");
 
 	private final String message;
 	private final String code;
