c6fbf1831c344f76377ae7293e6606b44db3fa22
hs12 <loveanna1228@gmail.com>
2024-08-20 19:06:30 +0900

    [refactor] : 클래스 레벨 requestMapping 제거

--- src/main/java/com/dnd/gongmuin/answer/controller/AnswerController.java
+++ src/main/java/com/dnd/gongmuin/answer/controller/AnswerController.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.dnd.gongmuin.answer.dto.AnswerDetailResponse;
@@ -24,13 +23,12 @@ import lombok.RequiredArgsConstructor;
 @Tag(name = "답변 API")
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/question-posts")
 public class AnswerController {
 	private final AnswerService answerService;
 
 	@Operation(summary = "답변 등록 API", description = "질문글에 대한 답변을 작성한다.")
 	@ApiResponse(useReturnTypeSchema = true)
-	@PostMapping("/{questionPostId}/answers")
+	@PostMapping("/api/question-posts/{questionPostId}/answers")
 	public ResponseEntity<AnswerDetailResponse> registerAnswer(
 		@PathVariable Long questionPostId,
 		@Valid @RequestBody RegisterAnswerRequest request,
@@ -42,7 +40,7 @@ public class AnswerController {
 
 	@Operation(summary = "답변 조회 API", description = "질문글에 속하는 답변을 모두 조회한다.")
 	@ApiResponse(useReturnTypeSchema = true)
-	@GetMapping("/{questionPostId}/answers")
+	@GetMapping("/api/question-posts/{questionPostId}/answers")
 	public ResponseEntity<PageResponse<AnswerDetailResponse>> getAnswersByQuestionPostId(
 		@PathVariable Long questionPostId
 	) {
@@ -52,7 +50,7 @@ public class AnswerController {
 
 	@Operation(summary = "답변 채택 API", description = "질문자가 답변을 채택한다.")
 	@ApiResponse(useReturnTypeSchema = true)
-	@PostMapping("/answers/{answerId}")
+	@PostMapping("/api/question-posts/answers/{answerId}")
 	public ResponseEntity<AnswerDetailResponse> getAnswersByQuestionPostId(
 		@PathVariable Long answerId,
 		@AuthenticationPrincipal Member member
