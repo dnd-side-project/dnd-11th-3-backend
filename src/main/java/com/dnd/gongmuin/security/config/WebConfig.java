a804891392e70f15429e89dbb52081582a2f11b8
권영태 <56019823+dudxo@users.noreply.github.com>
2024-08-22 17:08:04 +0900

    [fix #69] 프론트 요청 시 쿠키 값을 읽지 못하는 issue 해결 (#70)
    * [feat] : Credentials 허가 추가
    
    * [feat] : samesite 정책 none 추가 및 secure 허가

--- src/main/java/com/dnd/gongmuin/security/config/WebConfig.java
+++ src/main/java/com/dnd/gongmuin/security/config/WebConfig.java
@@ -11,9 +11,10 @@ public class WebConfig implements WebMvcConfigurer {
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**")
-			.allowedOrigins("http://localhost:3000", "https://gongmuin.netlify.app/",
-				"https://gongmuin.site/", "http://localhost:8080")
+			.allowedOrigins("http://localhost:3000", "https://gongmuin.netlify.app",
+				"https://gongmuin.site", "http://localhost:8080")
 			.allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+			.allowCredentials(true)
 			.allowedHeaders("*")
 			.exposedHeaders("Set-Cookie", "Authorization")
 			.maxAge(3000);
