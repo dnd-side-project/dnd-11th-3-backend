b9d0ebdf02b2904d6256a3435a878f6a6c4b6beb
권영태 <56019823+dudxo@users.noreply.github.com>
2024-08-22 20:34:48 +0900

    [fix #69] 토큰 CORS 해결 (#73)
    * [feat] : Credentials 허가 추가
    
    * [feat] : samesite 정책 none 추가 및 secure 허가
    
    * [chore] : CD workflow 수정
    
    * [fix] : CORS 임시 전체허용
    
    * [fix] : allowedOrigins -> allowedOriginPatterns 변경
    
    * [feat] : @EnableWebMvc 추가
    
    * [fix] WebConfig 수정
    
    * [fix] SecurityConfig 수정
    
    * [fix] : SecurityConfig cors 중복 URL 제거
    
    * [chore] : CORS 에러 해결 후 CD workflows 원상복구

--- src/main/java/com/dnd/gongmuin/security/config/WebConfig.java
+++ src/main/java/com/dnd/gongmuin/security/config/WebConfig.java
@@ -2,16 +2,18 @@ package com.dnd.gongmuin.security.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
 
 	// Spring MVC cors 설정
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**")
-			.allowedOrigins("http://localhost:3000", "https://gongmuin.netlify.app",
+			.allowedOrigins("http://localhost:3000", "https://gongmuin.netlify.app/", "https://gongmuin.netlify.app",
 				"https://gongmuin.site", "http://localhost:8080")
 			.allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
 			.allowCredentials(true)
