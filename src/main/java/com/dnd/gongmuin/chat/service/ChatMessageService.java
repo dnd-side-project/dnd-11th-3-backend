99671ee3e12eba6f0d39f89edf16a5764ef2b203
hs12 <loveanna1228@gmail.com>
2024-08-29 17:41:42 +0900

    [feat] : 채팅 메시지 저장, 조회 비즈니스 로직 추가

---
+++ src/main/java/com/dnd/gongmuin/chat/service/ChatMessageService.java
@@ -0,0 +1,40 @@
+package com.dnd.gongmuin.chat.service;
+
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.dnd.gongmuin.chat.dto.ChatMapper;
+import com.dnd.gongmuin.chat.dto.ChatMessageRequest;
+import com.dnd.gongmuin.chat.dto.ChatMessageResponse;
+import com.dnd.gongmuin.chat.repository.ChatMessageRepository;
+import com.dnd.gongmuin.common.dto.PageMapper;
+import com.dnd.gongmuin.common.dto.PageResponse;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class ChatMessageService {
+
+	private final ChatMessageRepository chatMessageRepository;
+
+	@Transactional
+	public void saveChatMessage(
+		ChatMessageRequest request,
+		Long chatRoomId,
+		Long memberId
+	) {
+		chatMessageRepository.save(ChatMapper.toChatMessage(request, chatRoomId, memberId));
+	}
+
+	@Transactional(readOnly = true)
+	public PageResponse<ChatMessageResponse> getChatMessage(Long chatRoomId, Pageable pageable) {
+		Slice<ChatMessageResponse> responsePage = chatMessageRepository
+			.findByChatRoomIdOrderByCreatedAtDesc(chatRoomId, pageable)
+			.map(ChatMapper::toChatMessageResponse);
+		return PageMapper.toPageResponse(responsePage);
+	}
+
+}
