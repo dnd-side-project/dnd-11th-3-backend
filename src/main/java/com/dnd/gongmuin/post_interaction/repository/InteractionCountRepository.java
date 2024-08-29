280124ed29633aaf3af278a4e95f644df3f9a166
hs12 <loveanna1228@gmail.com>
2024-08-29 21:56:52 +0900

    [refactor] : repository 어노테이션 추가

--- src/main/java/com/dnd/gongmuin/post_interaction/repository/InteractionCountRepository.java
+++ src/main/java/com/dnd/gongmuin/post_interaction/repository/InteractionCountRepository.java
@@ -3,10 +3,12 @@ package com.dnd.gongmuin.post_interaction.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.dnd.gongmuin.post_interaction.domain.InteractionCount;
 import com.dnd.gongmuin.post_interaction.domain.InteractionType;
 
+@Repository
 public interface InteractionCountRepository extends JpaRepository<InteractionCount, Long> {
 	Optional<InteractionCount> findByQuestionPostIdAndType(
 		Long questionPostId,
