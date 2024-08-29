c3702bb4173bd22387e633af6ac54061754d09f1
hs12 <loveanna1228@gmail.com>
2024-08-29 17:24:16 +0900

    [feat] : chatMessage 엔티티 -> 몽고 DB collection으로 변경

--- src/main/java/com/dnd/gongmuin/chat/domain/ChatMessage.java
+++ src/main/java/com/dnd/gongmuin/chat/domain/ChatMessage.java
@@ -1,69 +1,60 @@
 package com.dnd.gongmuin.chat.domain;
 
-import static jakarta.persistence.ConstraintMode.*;
-import static jakarta.persistence.EnumType.*;
-import static jakarta.persistence.FetchType.*;
-import static jakarta.persistence.GenerationType.*;
 import static lombok.AccessLevel.*;
 
-import com.dnd.gongmuin.common.entity.TimeBaseEntity;
-import com.dnd.gongmuin.member.domain.Member;
+import java.time.LocalDateTime;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.ForeignKey;
-import jakarta.persistence.GeneratedValue;
+import org.springframework.data.mongodb.core.mapping.Document;
+
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-@Entity
+@Document(collection = "chat_messages")
 @Getter
 @NoArgsConstructor(access = PROTECTED)
-public class ChatMessage extends TimeBaseEntity {
+public class ChatMessage {
 
 	@Id
-	@GeneratedValue(strategy = IDENTITY)
-	@Column(name = "chat_message_id")
-	private Long id;
+	private String id;
 
-	@Column(name = "content", nullable = false)
 	private String content;
 
-	@Column(name = "is_read", nullable = false)
+	private long chatRoomId;
+
+	private long memberId;
+
 	private Boolean isRead;
 
-	@Column(name = "media_url", nullable = false)
 	private String mediaUrl;
 
-	@Enumerated(STRING)
-	@Column(name = "type", nullable = false)
 	private MessageType type;
 
-	@ManyToOne(fetch = LAZY)
-	@JoinColumn(name = "member_id",
-		nullable = false,
-		foreignKey = @ForeignKey(NO_CONSTRAINT))
-	private Member member;
+	private LocalDateTime createdAt;
 
-	@ManyToOne(fetch = LAZY)
-	@JoinColumn(name = "chat_room_id",
-		nullable = false,
-		foreignKey = @ForeignKey(NO_CONSTRAINT))
-	private ChatRoom chatRoom;
-
-	@Builder
-	public ChatMessage(String content, String mediaUrl, MessageType type, Member member,
-		ChatRoom chatRoom) {
-		this.isRead = false;
+	private ChatMessage(
+		String content,
+		long chatRoomId,
+		long memberId,
+		String mediaUrl,
+		MessageType type
+	) {
 		this.content = content;
+		this.chatRoomId = chatRoomId;
+		this.memberId = memberId;
 		this.mediaUrl = mediaUrl;
 		this.type = type;
-		this.member = member;
-		this.chatRoom = chatRoom;
+		this.isRead = false;
+		this.createdAt = LocalDateTime.now();
+	}
+
+	public static ChatMessage of(
+		String content,
+		long chatRoomId,
+		long memberId,
+		String mediaUrl,
+		MessageType type
+	) {
+		return new ChatMessage(content, chatRoomId, memberId, mediaUrl, type);
 	}
 }
