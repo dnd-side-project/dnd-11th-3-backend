dd8fbd52ab13b67ec0d8ed8a9bd80c2fb8d9d9fc
dudxo <father8475@naver.com>
2024-09-01 12:24:17 +0900

    [test] : 알림 저장 비즈니스 로직 단위테스트 작성

---
+++ src/test/java/com/dnd/gongmuin/notification/NotificationServiceTest.java
@@ -0,0 +1,56 @@
+package com.dnd.gongmuin.notification;
+
+import static org.mockito.Mockito.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.util.ReflectionTestUtils;
+
+import com.dnd.gongmuin.common.fixture.MemberFixture;
+import com.dnd.gongmuin.common.fixture.QuestionPostFixture;
+import com.dnd.gongmuin.member.domain.Member;
+import com.dnd.gongmuin.notification.domain.Notification;
+import com.dnd.gongmuin.notification.domain.NotificationType;
+import com.dnd.gongmuin.notification.repository.NotificationRepository;
+import com.dnd.gongmuin.notification.service.NotificationService;
+import com.dnd.gongmuin.question_post.domain.QuestionPost;
+
+@ExtendWith(MockitoExtension.class)
+class NotificationServiceTest {
+
+	@Mock
+	NotificationRepository notificationRepository;
+
+	@InjectMocks
+	NotificationService notificationService;
+
+	@DisplayName("타겟 타입에 맞는 알림을 만들고 저장한다.")
+	@Test
+	void saveNotificationFromTarget() {
+		// given
+		Member member1 = MemberFixture.member();
+		Member member2 = MemberFixture.member2();
+		ReflectionTestUtils.setField(member1, "id", 1L);
+		ReflectionTestUtils.setField(member2, "id", 2L);
+
+		QuestionPost questionPost = QuestionPostFixture.questionPost(member1);
+		ReflectionTestUtils.setField(questionPost, "id", 1L);
+
+		// when
+		notificationService.saveNotificationFromTarget("답변", questionPost.getId(), member2.getId(), member1);
+
+		// then
+		verify(notificationRepository).save(any(Notification.class));
+		verify(notificationRepository).save(argThat(notification ->
+			notification.getType().equals(NotificationType.ANSWER) &&
+				notification.getTargetId().equals(questionPost.getId()) &&
+				notification.getTriggerMemberId().equals(member2.getId()) &&
+				notification.getMember().equals(member1)
+		));
+
+	}
+}
