5b1e37b5af8bbe02563373ae52347d2fef52acc4
dudxo <father8475@naver.com>
2024-09-01 13:04:45 +0900

    [feat] : 알림 목록 관련 에러코드 추가

--- src/main/java/com/dnd/gongmuin/notification/exception/NotificationErrorCode.java
+++ src/main/java/com/dnd/gongmuin/notification/exception/NotificationErrorCode.java
@@ -10,7 +10,8 @@ import lombok.RequiredArgsConstructor;
 public enum NotificationErrorCode implements ErrorCode {
 
 	NOT_FOUND_NOTIFICATION_TYPE("알맞은 알림 타입을 찾을 수 없습니다.", "NOTIFICATION_001"),
-	SAVE_NOTIFICATION_FAILED("알림 저장을 실패했습니다.", "NOTIFICATION_002");
+	SAVE_NOTIFICATION_FAILED("알림 저장을 실패했습니다.", "NOTIFICATION_002"),
+	NOTIFICATIONS_BY_MEMBER_FAILED("알림 목록을 불러오는데 실패했습니다.", "NOTIFICATION_003");
 
 	private final String message;
 	private final String code;
