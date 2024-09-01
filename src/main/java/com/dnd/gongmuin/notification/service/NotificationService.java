6d189879adcdbc24736acef8703ffac4b801e202
dudxo <father8475@naver.com>
2024-09-01 11:09:50 +0900

    [feat] : Notification 저장 로직 추가

--- src/main/java/com/dnd/gongmuin/notification/service/NotificationService.java
+++ src/main/java/com/dnd/gongmuin/notification/service/NotificationService.java
@@ -2,6 +2,9 @@ package com.dnd.gongmuin.notification.service;
 
 import org.springframework.stereotype.Service;
 
+import com.dnd.gongmuin.member.domain.Member;
+import com.dnd.gongmuin.notification.domain.Notification;
+import com.dnd.gongmuin.notification.domain.NotificationType;
 import com.dnd.gongmuin.notification.repository.NotificationRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -11,4 +14,15 @@ import lombok.RequiredArgsConstructor;
 public class NotificationService {
 
 	private final NotificationRepository notificationRepository;
+
+	public void saveNotificationFromTarget(String targetType, Long targetId, Member toMember) {
+		NotificationType type = findTargetType(targetType);
+		Notification notification = Notification.of(type, targetId, toMember);
+
+		notificationRepository.save(notification);
+	}
+
+	private NotificationType findTargetType(String targetType) {
+		return NotificationType.of(targetType);
+	}
 }
