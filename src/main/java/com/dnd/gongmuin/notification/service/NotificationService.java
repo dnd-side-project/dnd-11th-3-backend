f13b1e17e1acb5a9c4bc38cb87fb94ed89c99bde
dudxo <father8475@naver.com>
2024-09-01 11:18:29 +0900

    [feat] : 트랜잭션, 예외처리 추가

--- src/main/java/com/dnd/gongmuin/notification/service/NotificationService.java
+++ src/main/java/com/dnd/gongmuin/notification/service/NotificationService.java
@@ -1,10 +1,13 @@
 package com.dnd.gongmuin.notification.service;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.dnd.gongmuin.common.exception.runtime.ValidationException;
 import com.dnd.gongmuin.member.domain.Member;
 import com.dnd.gongmuin.notification.domain.Notification;
 import com.dnd.gongmuin.notification.domain.NotificationType;
+import com.dnd.gongmuin.notification.exception.NotificationErrorCode;
 import com.dnd.gongmuin.notification.repository.NotificationRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -15,11 +18,15 @@ public class NotificationService {
 
 	private final NotificationRepository notificationRepository;
 
+	@Transactional
 	public void saveNotificationFromTarget(String targetType, Long targetId, Member toMember) {
 		NotificationType type = findTargetType(targetType);
 		Notification notification = Notification.of(type, targetId, toMember);
-
-		notificationRepository.save(notification);
+		try {
+			notificationRepository.save(notification);
+		} catch (Exception e) {
+			throw new ValidationException(NotificationErrorCode.SAVE_NOTIFICATION_FAILED);
+		}
 	}
 
 	private NotificationType findTargetType(String targetType) {
