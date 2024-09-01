801e01a9b830912212309c9df4d56092d3effa06
dudxo <father8475@naver.com>
2024-09-01 11:15:41 +0900

    [feat] : Notification ErrorCode 추가

---
+++ src/main/java/com/dnd/gongmuin/notification/exception/NotificationErrorCode.java
@@ -0,0 +1,17 @@
+package com.dnd.gongmuin.notification.exception;
+
+import com.dnd.gongmuin.common.exception.ErrorCode;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum NotificationErrorCode implements ErrorCode {
+
+	NOT_FOUND_NOTIFICATION_TYPE("알맞은 알림 타입을 찾을 수 없습니다.", "NOTIFICATION_001"),
+	SAVE_NOTIFICATION_FAILED("알림 저장을 실패했습니다.", "NOTIFICATION_002");
+
+	private final String message;
+	private final String code;
+}
