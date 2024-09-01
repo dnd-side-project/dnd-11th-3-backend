d48d0f45a728b725568cee2dceb77dffd1b4b253
dudxo <father8475@naver.com>
2024-09-01 13:03:53 +0900

    [feat] : 알림 목록 API 추가

---
+++ src/main/java/com/dnd/gongmuin/notification/controller/NotificationController.java
@@ -0,0 +1,36 @@
+package com.dnd.gongmuin.notification.controller;
+
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.dnd.gongmuin.common.dto.PageResponse;
+import com.dnd.gongmuin.member.domain.Member;
+import com.dnd.gongmuin.notification.dto.response.NotificationsResponse;
+import com.dnd.gongmuin.notification.service.NotificationService;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+
+@Tag(name = "알림 API", description = "알림 API")
+@RestController
+@RequiredArgsConstructor
+public class NotificationController {
+
+	private final NotificationService notificationService;
+
+	@GetMapping("/api/notifications")
+	public ResponseEntity<PageResponse<NotificationsResponse>> getNotificationsByMember(
+		@RequestParam("type") String type,
+		@AuthenticationPrincipal Member member,
+		Pageable pageable) {
+
+		PageResponse<NotificationsResponse> response =
+			notificationService.getNotificationsByMember(type, member, pageable);
+
+		return ResponseEntity.ok(response);
+	}
+}
