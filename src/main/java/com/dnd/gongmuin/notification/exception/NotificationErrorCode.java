a9e2a8e3d54fa8d5ea0dd12bc676a9d1aa4e0bd8
dudxo <father8475@naver.com>
2024-09-01 21:16:37 +0900

    [feat] : 알림 소유주 관련 에러코드 추가

--- src/main/java/com/dnd/gongmuin/notification/exception/NotificationErrorCode.java
+++ src/main/java/com/dnd/gongmuin/notification/exception/NotificationErrorCode.java
@@ -13,7 +13,8 @@ public enum NotificationErrorCode implements ErrorCode {
 	SAVE_NOTIFICATION_FAILED("알림 저장을 실패했습니다.", "NOTIFICATION_002"),
 	NOTIFICATIONS_BY_MEMBER_FAILED("알림 목록을 불러오는데 실패했습니다.", "NOTIFICATION_003"),
 	NOT_FOUND_NOTIFICATION("해당 알림을 찾을 수 없습니다", "NOTIFICATION_004"),
-	CHANGE_IS_READ_NOTIFICATION_FAILED("해당 알림의 읽음 여부 변경을 실패했습니다.", "NOTIFICATION_005");
+	CHANGE_IS_READ_NOTIFICATION_FAILED("해당 알림의 읽음 여부 변경을 실패했습니다.", "NOTIFICATION_005"),
+	INVALID_NOTIFICATION_OWNER("해당 알림의 주인이 아닙니다.", "NOTIFICATION_006");
 
 	private final String message;
 	private final String code;
