e7df8db2055b8b3190a92b147e30e555cb7cef1d
dudxo <father8475@naver.com>
2024-09-01 20:18:48 +0900

    [feat] : 알림 읽기 관련 에러코드 추가

--- src/main/java/com/dnd/gongmuin/notification/exception/NotificationErrorCode.java
+++ src/main/java/com/dnd/gongmuin/notification/exception/NotificationErrorCode.java
@@ -11,7 +11,9 @@ public enum NotificationErrorCode implements ErrorCode {
 
 	NOT_FOUND_NOTIFICATION_TYPE("알맞은 알림 타입을 찾을 수 없습니다.", "NOTIFICATION_001"),
 	SAVE_NOTIFICATION_FAILED("알림 저장을 실패했습니다.", "NOTIFICATION_002"),
-	NOTIFICATIONS_BY_MEMBER_FAILED("알림 목록을 불러오는데 실패했습니다.", "NOTIFICATION_003");
+	NOTIFICATIONS_BY_MEMBER_FAILED("알림 목록을 불러오는데 실패했습니다.", "NOTIFICATION_003"),
+	NOT_FOUND_NOTIFICATION("해당 알림을 찾을 수 없습니다", "NOTIFICATION_004"),
+	CHANGE_IS_READ_NOTIFICATION_FAILED("해당 알림의 읽음 여부 변경을 실패했습니다.", "NOTIFICATION_005");
 
 	private final String message;
 	private final String code;
