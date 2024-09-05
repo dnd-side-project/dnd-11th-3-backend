7dcd1ee0d4cfa36913fdff72d2ea39d0edf2420a
dudxo <father8475@naver.com>
2024-09-05 19:02:26 +0900

    [style] : 안쓰는 예외 throws 제거

--- src/main/java/com/dnd/gongmuin/security/handler/CustomOauth2FailureHandler.java
+++ src/main/java/com/dnd/gongmuin/security/handler/CustomOauth2FailureHandler.java
@@ -6,7 +6,6 @@ import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.stereotype.Component;
 
-import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
@@ -17,7 +16,7 @@ public class CustomOauth2FailureHandler implements AuthenticationFailureHandler 
 
 	@Override
 	public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response,
-		AuthenticationException exception) throws IOException, ServletException {
+		AuthenticationException exception) throws IOException {
 		log.error("소셜 로그인 실패", exception);
 		response.sendError(HttpServletResponse.SC_BAD_REQUEST, "소셜 로그인에 실패하였습니다.");
 	}
