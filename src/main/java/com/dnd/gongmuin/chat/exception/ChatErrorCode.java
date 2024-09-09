0cbb486b7d05863996150f9bd0e8f22370c97365
hs12 <loveanna1228@gmail.com>
2024-09-09 22:29:41 +0900

    [feat] : 채팅방 에러코드 추가

--- src/main/java/com/dnd/gongmuin/chat/exception/ChatErrorCode.java
+++ src/main/java/com/dnd/gongmuin/chat/exception/ChatErrorCode.java
@@ -9,7 +9,8 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum ChatErrorCode implements ErrorCode {
 
-	INVALID_MESSAGE_TYPE("메시지 타입을 올바르게 입력해주세요.", "CH_001");
+	INVALID_MESSAGE_TYPE("메시지 타입을 올바르게 입력해주세요.", "CH_001"),
+	NOT_FOUND_CHAT_ROOM("해당 아이디의 채팅방이 존재하지 않습니다.", "CH_002");
 
 	private final String message;
 	private final String code;
