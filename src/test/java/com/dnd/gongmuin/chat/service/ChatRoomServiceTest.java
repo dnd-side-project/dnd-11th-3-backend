876af4c51001f187ae1d4032f0d23607a6870970
hs12 <loveanna1228@gmail.com>
2024-09-13 15:44:56 +0900

    [test] : 채팅방 생성 비즈니스 로직 테스트

--- src/test/java/com/dnd/gongmuin/chat/service/ChatRoomServiceTest.java
+++ src/test/java/com/dnd/gongmuin/chat/service/ChatRoomServiceTest.java
@@ -5,6 +5,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.BDDMockito.*;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -16,9 +17,20 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.SliceImpl;
 
 import com.dnd.gongmuin.chat.domain.ChatMessage;
+import com.dnd.gongmuin.chat.domain.ChatRoom;
+import com.dnd.gongmuin.chat.dto.request.CreateChatRoomRequest;
 import com.dnd.gongmuin.chat.dto.response.ChatMessageResponse;
+import com.dnd.gongmuin.chat.dto.response.ChatRoomDetailResponse;
 import com.dnd.gongmuin.chat.repository.ChatMessageRepository;
+import com.dnd.gongmuin.chat.repository.ChatRoomRepository;
 import com.dnd.gongmuin.common.fixture.ChatMessageFixture;
+import com.dnd.gongmuin.common.fixture.ChatRoomFixture;
+import com.dnd.gongmuin.common.fixture.MemberFixture;
+import com.dnd.gongmuin.common.fixture.QuestionPostFixture;
+import com.dnd.gongmuin.member.domain.Member;
+import com.dnd.gongmuin.member.repository.MemberRepository;
+import com.dnd.gongmuin.question_post.domain.QuestionPost;
+import com.dnd.gongmuin.question_post.repository.QuestionPostRepository;
 
 @DisplayName("[채팅방 메시지 서비스 단위 테스트]")
 @ExtendWith(MockitoExtension.class)
@@ -28,6 +40,15 @@ class ChatRoomServiceTest {
 
 	@Mock
 	private ChatMessageRepository chatMessageRepository;
+
+	@Mock
+	private ChatRoomRepository chatRoomRepository;
+
+	@Mock
+	private MemberRepository memberRepository;
+
+	@Mock
+	private QuestionPostRepository questionPostRepository;
 
 	@InjectMocks
 	private ChatRoomService chatRoomService;
@@ -46,6 +67,36 @@ class ChatRoomServiceTest {
 		//then
 		assertAll(
 			() -> assertThat(response).hasSize(1)
+		);
+	}
+
+	@DisplayName("[요청자가 채팅방을 생성할 수 있다.]")
+	@Test
+	void createChatRoom() {
+		//given
+		Member inquirer = MemberFixture.member(1L);
+		Member answerer = MemberFixture.member(2L);
+		QuestionPost questionPost = QuestionPostFixture.questionPost(inquirer);
+		CreateChatRoomRequest request = new CreateChatRoomRequest(
+			questionPost.getId(),
+			answerer.getId()
+		);
+		ChatRoom chatRoom = ChatRoomFixture.chatRoom(questionPost, inquirer, answerer);
+
+		given(questionPostRepository.findById(questionPost.getId()))
+			.willReturn(Optional.of(questionPost));
+		given(memberRepository.findById(answerer.getId()))
+			.willReturn(Optional.of(answerer));
+		given(chatRoomRepository.save(any(ChatRoom.class)))
+			.willReturn(chatRoom);
+
+		//when
+		ChatRoomDetailResponse response = chatRoomService.createChatRoom(request, inquirer);
+
+		//then
+		assertAll(
+			() -> assertThat(response.questionPostId()).isEqualTo(request.questionPostId()),
+			() -> assertThat(response.receiverInfo().memberId()).isEqualTo(request.answererId())
 		);
 	}
 }
