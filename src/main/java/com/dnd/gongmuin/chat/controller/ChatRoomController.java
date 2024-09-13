65f40ffcfa30c1add440638f1c1a2c2c9598e9f1
hs12 <loveanna1228@gmail.com>
2024-09-13 15:48:38 +0900

    [feat] : 채팅방 생성 API 메서드 추가

--- src/main/java/com/dnd/gongmuin/chat/controller/ChatRoomController.java
+++ src/main/java/com/dnd/gongmuin/chat/controller/ChatRoomController.java
@@ -2,16 +2,22 @@ package com.dnd.gongmuin.chat.controller;
 
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.dnd.gongmuin.chat.dto.ChatMessageResponse;
+import com.dnd.gongmuin.chat.dto.response.ChatMessageResponse;
+import com.dnd.gongmuin.chat.dto.response.ChatRoomDetailResponse;
+import com.dnd.gongmuin.chat.dto.request.CreateChatRoomRequest;
 import com.dnd.gongmuin.chat.service.ChatRoomService;
 import com.dnd.gongmuin.common.dto.PageResponse;
+import com.dnd.gongmuin.member.domain.Member;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 @Tag(name = "채팅방 API")
@@ -29,6 +35,16 @@ public class ChatRoomController {
 	) {
 		PageResponse<ChatMessageResponse> response =
 			chatRoomService.getChatMessages(chatRoomId, pageable);
+		return ResponseEntity.ok(response);
+	}
+
+	@Operation(summary = "채팅방 생성 API", description = "요청자가 답변자와의 채팅방을 생성한다.")
+	@PostMapping("/api/chat-rooms")
+	public ResponseEntity<ChatRoomDetailResponse> createChatRoom(
+		@Valid CreateChatRoomRequest request,
+		@AuthenticationPrincipal Member member
+	) {
+		ChatRoomDetailResponse response = chatRoomService.createChatRoom(request, member);
 		return ResponseEntity.ok(response);
 	}
 }
