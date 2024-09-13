bc6c8079eba76c117e91d374ab47b0d948c80b19
hs12 <loveanna1228@gmail.com>
2024-09-13 15:23:19 +0900

    [feat] : 채팅방 생성 비즈니스 로직 추가

--- src/main/java/com/dnd/gongmuin/chat/service/ChatRoomService.java
+++ src/main/java/com/dnd/gongmuin/chat/service/ChatRoomService.java
@@ -5,11 +5,22 @@ import org.springframework.data.domain.Slice;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.dnd.gongmuin.chat.dto.ChatMapper;
-import com.dnd.gongmuin.chat.dto.ChatMessageResponse;
+import com.dnd.gongmuin.chat.dto.ChatMessageMapper;
+import com.dnd.gongmuin.chat.dto.ChatRoomMapper;
+import com.dnd.gongmuin.chat.dto.response.ChatMessageResponse;
+import com.dnd.gongmuin.chat.dto.response.ChatRoomDetailResponse;
+import com.dnd.gongmuin.chat.dto.request.CreateChatRoomRequest;
 import com.dnd.gongmuin.chat.repository.ChatMessageRepository;
+import com.dnd.gongmuin.chat.repository.ChatRoomRepository;
 import com.dnd.gongmuin.common.dto.PageMapper;
 import com.dnd.gongmuin.common.dto.PageResponse;
+import com.dnd.gongmuin.common.exception.runtime.NotFoundException;
+import com.dnd.gongmuin.member.domain.Member;
+import com.dnd.gongmuin.member.exception.MemberErrorCode;
+import com.dnd.gongmuin.member.repository.MemberRepository;
+import com.dnd.gongmuin.question_post.domain.QuestionPost;
+import com.dnd.gongmuin.question_post.exception.QuestionPostErrorCode;
+import com.dnd.gongmuin.question_post.repository.QuestionPostRepository;
 
 import lombok.RequiredArgsConstructor;
 
@@ -18,13 +29,34 @@ import lombok.RequiredArgsConstructor;
 public class ChatRoomService {
 
 	private final ChatMessageRepository chatMessageRepository;
+	private final ChatRoomRepository chatRoomRepository;
+	private final MemberRepository memberRepository;
+	private final QuestionPostRepository questionPostRepository;
 
 	@Transactional(readOnly = true)
 	public PageResponse<ChatMessageResponse> getChatMessages(Long chatRoomId, Pageable pageable) {
 		Slice<ChatMessageResponse> responsePage = chatMessageRepository
 			.findByChatRoomIdOrderByCreatedAtDesc(chatRoomId, pageable)
-			.map(ChatMapper::toChatMessageResponse);
+			.map(ChatMessageMapper::toChatMessageResponse);
 		return PageMapper.toPageResponse(responsePage);
 	}
 
+	@Transactional
+	public ChatRoomDetailResponse createChatRoom(CreateChatRoomRequest request, Member inquirer) {
+		QuestionPost questionPost = getQuestionPostById(request.questionPostId());
+		Member answerer = getMemberById(request.answererId());
+		return ChatRoomMapper.toChatRoomDetailResponse(
+			chatRoomRepository.save(ChatRoomMapper.toChatRoom(questionPost, inquirer, answerer))
+		);
+	}
+
+	private QuestionPost getQuestionPostById(Long id){
+		return questionPostRepository.findById(id)
+			.orElseThrow(() -> new NotFoundException(QuestionPostErrorCode.NOT_FOUND_QUESTION_POST));
+	}
+	private Member getMemberById(Long id){
+		return memberRepository.findById(id)
+			.orElseThrow(() -> new NotFoundException(MemberErrorCode.NOT_FOUND_MEMBER));
+	}
 }
+
