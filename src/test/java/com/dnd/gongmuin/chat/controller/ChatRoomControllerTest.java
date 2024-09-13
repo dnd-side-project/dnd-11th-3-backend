d20989bbb6ef58b8303d9d385e623ddc3a84920c
hs12 <loveanna1228@gmail.com>
2024-09-13 16:12:55 +0900

    [test] : 채팅방 생성 통합 테스트

--- src/test/java/com/dnd/gongmuin/chat/controller/ChatRoomControllerTest.java
+++ src/test/java/com/dnd/gongmuin/chat/controller/ChatRoomControllerTest.java
@@ -1,5 +1,6 @@
 package com.dnd.gongmuin.chat.controller;
 
+import static org.springframework.http.MediaType.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -10,15 +11,28 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.dnd.gongmuin.chat.domain.ChatMessage;
+import com.dnd.gongmuin.chat.dto.request.CreateChatRoomRequest;
 import com.dnd.gongmuin.chat.repository.ChatMessageRepository;
 import com.dnd.gongmuin.common.fixture.ChatMessageFixture;
+import com.dnd.gongmuin.common.fixture.MemberFixture;
+import com.dnd.gongmuin.common.fixture.QuestionPostFixture;
 import com.dnd.gongmuin.common.support.ApiTestSupport;
+import com.dnd.gongmuin.member.domain.Member;
+import com.dnd.gongmuin.member.repository.MemberRepository;
+import com.dnd.gongmuin.question_post.domain.QuestionPost;
+import com.dnd.gongmuin.question_post.repository.QuestionPostRepository;
 
 @DisplayName("[ChatMessage 통합 테스트]")
 class ChatRoomControllerTest extends ApiTestSupport {
 
 	@Autowired
 	private ChatMessageRepository chatMessageRepository;
+
+	@Autowired
+	private MemberRepository memberRepository;
+
+	@Autowired
+	private QuestionPostRepository questionPostRepository;
 
 	@DisplayName("[채팅방 아이디로 메시지를 조회할 수 있다.]")
 	@Test
@@ -35,4 +49,19 @@ class ChatRoomControllerTest extends ApiTestSupport {
 			.andExpect(jsonPath("$.content[0].content").value(chatMessages.get(0).getContent()))
 			.andExpect(jsonPath("$.content[0].type").value(chatMessages.get(0).getType().getLabel()));
 	}
+
+	@DisplayName("[채팅방을 생성할 수 있다.]")
+	@Test
+	void createChatRoom() throws Exception {
+		Member answerer = memberRepository.save(MemberFixture.member4());
+		QuestionPost questionPost = questionPostRepository.save(QuestionPostFixture.questionPost(loginMember));
+		CreateChatRoomRequest request = new CreateChatRoomRequest(questionPost.getId(), answerer.getId());
+
+		mockMvc.perform(post("/api/chat-rooms")
+				.cookie(accessToken)
+				.content(toJson(request))
+				.contentType(APPLICATION_JSON))
+			.andExpect(status().isOk());
+	}
+
 }
