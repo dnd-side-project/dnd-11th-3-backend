011d4d339d5b98f06d82e35a03fdce7bf6a97b5c
hs12 <loveanna1228@gmail.com>
2024-09-14 15:33:11 +0900

    [feat] : dto 메시지 수정

--- src/main/java/com/dnd/gongmuin/chat/dto/request/CreateChatRoomRequest.java
+++ src/main/java/com/dnd/gongmuin/chat/dto/request/CreateChatRoomRequest.java
@@ -3,10 +3,10 @@ package com.dnd.gongmuin.chat.dto.request;
 import jakarta.validation.constraints.NotNull;
 
 public record CreateChatRoomRequest(
-	@NotNull(message = "질문 게시글 아이디를 입력해주세요.")
+	@NotNull(message = "질문 게시글 아이디는 필수 입력 항목입니다.")
 	Long questionPostId,
 
-	@NotNull(message = "답변자 아이디를 입력해주세요.")
+	@NotNull(message = "답변 아이디는 필수 입력 항목입니다.")
 	Long answererId
 ) {
 }
