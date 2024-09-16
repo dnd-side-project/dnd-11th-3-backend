4495385716dbe5e7dcc87f4b57e931cb7bf83ca5
hs12 <loveanna1228@gmail.com>
2024-09-16 21:19:38 +0900

    [test] : 채팅방 수락 로직 통합테스트

--- src/test/java/com/dnd/gongmuin/chat/controller/ChatRoomControllerTest.java
+++ src/test/java/com/dnd/gongmuin/chat/controller/ChatRoomControllerTest.java
@@ -11,9 +11,13 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.dnd.gongmuin.chat.domain.ChatMessage;
+import com.dnd.gongmuin.chat.domain.ChatRoom;
+import com.dnd.gongmuin.chat.domain.ChatStatus;
 import com.dnd.gongmuin.chat.dto.request.CreateChatRoomRequest;
 import com.dnd.gongmuin.chat.repository.ChatMessageRepository;
+import com.dnd.gongmuin.chat.repository.ChatRoomRepository;
 import com.dnd.gongmuin.common.fixture.ChatMessageFixture;
+import com.dnd.gongmuin.common.fixture.ChatRoomFixture;
 import com.dnd.gongmuin.common.fixture.MemberFixture;
 import com.dnd.gongmuin.common.fixture.QuestionPostFixture;
 import com.dnd.gongmuin.common.support.ApiTestSupport;
@@ -25,6 +29,8 @@ import com.dnd.gongmuin.question_post.repository.QuestionPostRepository;
 @DisplayName("[ChatMessage 통합 테스트]")
 class ChatRoomControllerTest extends ApiTestSupport {
 
+	private static final int CHAT_REWARD = 2000;
+
 	@Autowired
 	private ChatMessageRepository chatMessageRepository;
 
@@ -33,6 +39,9 @@ class ChatRoomControllerTest extends ApiTestSupport {
 
 	@Autowired
 	private QuestionPostRepository questionPostRepository;
+
+	@Autowired
+	private ChatRoomRepository chatRoomRepository;
 
 	@DisplayName("[채팅방 아이디로 메시지를 조회할 수 있다.]")
 	@Test
@@ -64,4 +73,18 @@ class ChatRoomControllerTest extends ApiTestSupport {
 			.andExpect(status().isOk());
 	}
 
+	@DisplayName("[답변자가 채팅 요청을 수락할 수 있다.]")
+	@Test
+	void acceptChatRoom() throws Exception {
+		Member inquirer = memberRepository.save(MemberFixture.member4());
+		QuestionPost questionPost = questionPostRepository.save(QuestionPostFixture.questionPost(inquirer));
+		ChatRoom chatRoom = chatRoomRepository.save(ChatRoomFixture.chatRoom(questionPost, inquirer, loginMember));
+		int previousAnswererCredit = chatRoom.getAnswerer().getCredit();
+
+		mockMvc.perform(patch("/api/chat-rooms/{chatRoomId}/accept", 1L)
+				.cookie(accessToken))
+			.andExpect(status().isOk())
+			.andExpect(jsonPath("$.chatStatus").value(ChatStatus.ACCEPTED.getLabel()))
+			.andExpect(jsonPath("$.credit").value(previousAnswererCredit + CHAT_REWARD));
+	}
 }
