1200c169231bf75edf6136e83453ffff26542023
hs12 <loveanna1228@gmail.com>
2024-09-16 20:10:22 +0900

    [feat] : 채팅 수락 컨트롤러 메서드 작성

--- src/main/java/com/dnd/gongmuin/chat/controller/ChatRoomController.java
+++ src/main/java/com/dnd/gongmuin/chat/controller/ChatRoomController.java
@@ -4,12 +4,14 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.dnd.gongmuin.chat.dto.request.CreateChatRoomRequest;
+import com.dnd.gongmuin.chat.dto.response.AcceptChatResponse;
 import com.dnd.gongmuin.chat.dto.response.ChatMessageResponse;
 import com.dnd.gongmuin.chat.dto.response.ChatRoomDetailResponse;
 import com.dnd.gongmuin.chat.service.ChatRoomService;
@@ -46,6 +48,16 @@ public class ChatRoomController {
 		@AuthenticationPrincipal Member member
 	) {
 		ChatRoomDetailResponse response = chatRoomService.createChatRoom(request, member);
+		return ResponseEntity.ok(response);
+	}
+
+	@Operation(summary = "채팅 수락 API", description = "채팅방에서 요청자와의 채팅을 수락한다.")
+	@PatchMapping("/api/chat-rooms/{chatRoomId}/accept")
+	public ResponseEntity<AcceptChatResponse> acceptChat(
+		@PathVariable("chatRoomId") Long chatRoomId,
+		@AuthenticationPrincipal Member member
+	) {
+		AcceptChatResponse response = chatRoomService.acceptChat(chatRoomId, member);
 		return ResponseEntity.ok(response);
 	}
 }
