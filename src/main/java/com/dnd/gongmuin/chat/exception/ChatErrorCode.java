6748dc4076d75d5bd893d595f844a65e2601d2b0
hs12 <loveanna1228@gmail.com>
2024-09-16 19:39:26 +0900

    [feat] : 채팅방 수락 관련 에러코드 추가

--- src/main/java/com/dnd/gongmuin/chat/exception/ChatErrorCode.java
+++ src/main/java/com/dnd/gongmuin/chat/exception/ChatErrorCode.java
@@ -10,7 +10,9 @@ import lombok.RequiredArgsConstructor;
 public enum ChatErrorCode implements ErrorCode {
 
 	INVALID_MESSAGE_TYPE("메시지 타입을 올바르게 입력해주세요.", "CH_001"),
-	NOT_FOUND_CHAT_ROOM("해당 아이디의 채팅방이 존재하지 않습니다.", "CH_002");
+	NOT_FOUND_CHAT_ROOM("해당 아이디의 채팅방이 존재하지 않습니다.", "CH_002"),
+	UNAUTHORIZED_REQUEST("채팅 수락을 하거나 거절할 권한이 없습니다.", "CH_003"),
+	UNABLE_TO_ACCEPT("이미 수락했거나 거절한 요청입니다.", "CH_004");
 
 	private final String message;
 	private final String code;
