128aa5517b74c65cd4360490711de27bab56c99c
hs12 <loveanna1228@gmail.com>
2024-09-27 19:21:37 +0900

    [feat] : 채팅방 목록 조회 DTO 추가

---
+++ src/main/java/com/dnd/gongmuin/chat/dto/response/ChatRoomSimpleResponse.java
@@ -0,0 +1,11 @@
+package com.dnd.gongmuin.chat.dto.response;
+
+import com.dnd.gongmuin.question_post.dto.response.MemberInfo;
+
+public record ChatRoomSimpleResponse (
+	Long chatRoomId,
+	MemberInfo chatPartner,
+	String latestMessage,
+	String messageType,
+	String messageCreatedAt
+){}
