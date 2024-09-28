31299e8e2bcfafd7d20b054c7abfc77311a6b058
hs12 <loveanna1228@gmail.com>
2024-09-28 16:47:06 +0900

    [feat] : 채팅방 상태 enum 변환로직 추가

--- src/main/java/com/dnd/gongmuin/chat/domain/ChatStatus.java
+++ src/main/java/com/dnd/gongmuin/chat/domain/ChatStatus.java
@@ -1,5 +1,10 @@
 package com.dnd.gongmuin.chat.domain;
 
+import java.util.Arrays;
+
+import com.dnd.gongmuin.chat.exception.ChatErrorCode;
+import com.dnd.gongmuin.common.exception.runtime.ValidationException;
+
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
@@ -12,4 +17,15 @@ public enum ChatStatus {
 	REJECTED("거절됨");
 
 	private final String label;
+
+	public static ChatStatus from(String input) {
+		return Arrays.stream(values())
+			.filter(status -> status.isEqual(input))
+			.findAny()
+			.orElseThrow(() -> new ValidationException(ChatErrorCode.NOT_FOUND_CHAT_STATUS));
+	}
+
+	private boolean isEqual(String input) {
+		return input.equals(this.label);
+	}
 }
