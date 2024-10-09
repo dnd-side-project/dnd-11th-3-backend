9b3cafd0b0d656732e31db43f9808bf556f5b64b
hs12 <loveanna1228@gmail.com>
2024-10-09 22:09:54 +0900

    [feat] : 크레딧 타입 추가

--- src/main/java/com/dnd/gongmuin/credit_history/domain/CreditType.java
+++ src/main/java/com/dnd/gongmuin/credit_history/domain/CreditType.java
@@ -11,8 +11,9 @@ public enum CreditType {
 
 	CHOOSE("채택하기", "출금"),
 	CHOSEN("채택받기", "입금"),
-	CHAT_REQUEST("채팅신청", "출금"),
-	CHAT_ACCEPT("채팅받기", "입금");
+	CHAT_REQUEST("채팅 요청", "출금"),
+	CHAT_ACCEPT("채팅 수락", "입금"),
+	CHAT_REFUND("채팅 환급", "입금");
 
 	private final String label;
 	private final String detail;
