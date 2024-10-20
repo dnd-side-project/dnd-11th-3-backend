0f62316ec3ea820b398ac644f796e17f8ab6370d
hs12 <loveanna1228@gmail.com>
2024-10-20 14:14:37 +0900

    [test] : 회원 크레딧 환급 메서드 chatRoomRepository -> memberRepository로 이동 테스트 반영

--- src/test/java/com/dnd/gongmuin/common/support/DataJpaTestSupport.java
+++ src/test/java/com/dnd/gongmuin/common/support/DataJpaTestSupport.java
@@ -1,5 +1,6 @@
 package com.dnd.gongmuin.common.support;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.ComponentScan;
@@ -9,9 +10,13 @@ import org.springframework.stereotype.Repository;
 import com.dnd.gongmuin.common.config.QueryDslConfig;
 import com.dnd.gongmuin.config.TestAuditingConfig;
 
+import jakarta.persistence.EntityManager;
+
 //repository용
 @DataJpaTest(includeFilters = @ComponentScan.Filter(Repository.class))
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @Import({TestAuditingConfig.class, QueryDslConfig.class})
 public abstract class DataJpaTestSupport extends TestContainerSupport {
+	@Autowired
+	protected EntityManager em;
 }
