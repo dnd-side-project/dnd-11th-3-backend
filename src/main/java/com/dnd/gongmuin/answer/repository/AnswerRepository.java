7c5f9e57887a1b73c05a2854fcdd66f9211225a1
hs12 <loveanna1228@gmail.com>
2024-11-11 20:56:26 +0900

    [style] : 코드 리포멧팅

--- src/main/java/com/dnd/gongmuin/answer/repository/AnswerRepository.java
+++ src/main/java/com/dnd/gongmuin/answer/repository/AnswerRepository.java
@@ -1,22 +1,24 @@
 package com.dnd.gongmuin.answer.repository;
 
-import com.dnd.gongmuin.answer.domain.Answer;
-import com.dnd.gongmuin.member.domain.Member;
 import java.util.List;
+
 import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import com.dnd.gongmuin.answer.domain.Answer;
+import com.dnd.gongmuin.member.domain.Member;
+
 @Repository
 public interface AnswerRepository extends JpaRepository<Answer, Long> {
 
-    Slice<Answer> findByQuestionPostId(Long questionPostId);
+	Slice<Answer> findByQuestionPostId(Long questionPostId);
 
-    List<Answer> findAllByMember(Member member);
+	List<Answer> findAllByMember(Member member);
 
-    @Modifying(flushAutomatically = true, clearAutomatically = true)
-    @Query("UPDATE Answer a SET a.member = :member WHERE a.member.id = :memberId")
-    public void updateAnswersMember(Long memberId, Member member);
+	@Modifying(flushAutomatically = true, clearAutomatically = true)
+	@Query("UPDATE Answer a SET a.member = :member WHERE a.member.id = :memberId")
+	void updateAnswersMember(Long memberId, Member member);
 }
--- src/main/java/com/dnd/gongmuin/notification/repository/NotificationRepository.java
+++ src/main/java/com/dnd/gongmuin/notification/repository/NotificationRepository.java
@@ -1,11 +1,12 @@
 package com.dnd.gongmuin.notification.repository;
 
-import com.dnd.gongmuin.member.domain.Member;
-import com.dnd.gongmuin.notification.domain.Notification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.dnd.gongmuin.member.domain.Member;
+import com.dnd.gongmuin.notification.domain.Notification;
+
 @Repository
 public interface NotificationRepository extends JpaRepository<Notification, Long>, NotificationCustom {
-    void deleteByMember(Member member);
+	void deleteByMember(Member member);
 }
