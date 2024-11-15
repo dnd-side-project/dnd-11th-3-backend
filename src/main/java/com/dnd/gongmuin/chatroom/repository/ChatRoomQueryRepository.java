56452917320098f25e43d8fd9c81f866dcd5ca45
hs12 <loveanna1228@gmail.com>
2024-11-15 22:42:59 +0900

    [feat] : 채팅방 -> 채팅요청 repository로 옮긴 메서드 삭제

--- src/main/java/com/dnd/gongmuin/chatroom/repository/ChatRoomQueryRepository.java
+++ src/main/java/com/dnd/gongmuin/chatroom/repository/ChatRoomQueryRepository.java
@@ -1,20 +1,12 @@
 package com.dnd.gongmuin.chatroom.repository;
 
-import java.util.List;
-
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 
-import com.dnd.gongmuin.chatroom.dto.response.ChatProposalInfo;
 import com.dnd.gongmuin.chatroom.dto.response.ChatRoomInfo;
 import com.dnd.gongmuin.member.domain.Member;
 
 public interface ChatRoomQueryRepository {
 
 	Slice<ChatRoomInfo> getChatRoomsByMember(Member member, Pageable pageable);
-	Slice<ChatProposalInfo> getChatProposalsByMember(Member member, Pageable pageable);
-
-	List<Long> getAutoRejectedInquirerIds();
-
-	void updateChatRoomStatusRejected();
 }
--- src/main/java/com/dnd/gongmuin/chatroom/repository/ChatRoomQueryRepositoryImpl.java
+++ src/main/java/com/dnd/gongmuin/chatroom/repository/ChatRoomQueryRepositoryImpl.java
@@ -1,21 +1,14 @@
 package com.dnd.gongmuin.chatroom.repository;
 
+import static com.dnd.gongmuin.chatroom.domain.QChatRoom.*;
 
-import static com.dnd.gongmuin.chat.domain.QChatRoom.*;
-
-import java.time.LocalDateTime;
 import java.util.List;
 
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.domain.SliceImpl;
 
-import com.dnd.gongmuin.chatroom.domain.InquiryStatus;
-import com.dnd.gongmuin.chatroom.dto.response.ChatProposalInfo;
 import com.dnd.gongmuin.chatroom.dto.response.ChatRoomInfo;
-import com.dnd.gongmuin.chat.dto.response.QChatProposalInfo;
-import com.dnd.gongmuin.chat.dto.response.QChatRoomInfo;
-import com.dnd.gongmuin.chatroom.domain.QChatRoom;
 import com.dnd.gongmuin.chatroom.dto.response.QChatRoomInfo;
 import com.dnd.gongmuin.member.domain.Member;
 import com.querydsl.core.types.dsl.CaseBuilder;
@@ -34,91 +27,32 @@ public class ChatRoomQueryRepositoryImpl implements ChatRoomQueryRepository {
 	) {
 		List<ChatRoomInfo> content = queryFactory
 			.select(new QChatRoomInfo(
-				QChatRoom.chatRoom.id,
+				chatRoom.id,
 				new CaseBuilder()
-					.when(QChatRoom.chatRoom.inquirer.id.eq(member.getId()))
-					.then(QChatRoom.chatRoom.answerer.id)
-					.otherwise(QChatRoom.chatRoom.inquirer.id),
+					.when(chatRoom.inquirer.id.eq(member.getId()))
+					.then(chatRoom.answerer.id)
+					.otherwise(chatRoom.inquirer.id),
 
 				new CaseBuilder()
-					.when(QChatRoom.chatRoom.inquirer.id.eq(member.getId()))
-					.then(QChatRoom.chatRoom.answerer.nickname)
-					.otherwise(QChatRoom.chatRoom.inquirer.nickname),
+					.when(chatRoom.inquirer.id.eq(member.getId()))
+					.then(chatRoom.answerer.nickname)
+					.otherwise(chatRoom.inquirer.nickname),
 				new CaseBuilder()
-					.when(QChatRoom.chatRoom.inquirer.id.eq(member.getId()))
-					.then(QChatRoom.chatRoom.answerer.jobGroup)
-					.otherwise(QChatRoom.chatRoom.inquirer.jobGroup),
+					.when(chatRoom.inquirer.id.eq(member.getId()))
+					.then(chatRoom.answerer.jobGroup)
+					.otherwise(chatRoom.inquirer.jobGroup),
 				new CaseBuilder()
-					.when(QChatRoom.chatRoom.inquirer.id.eq(member.getId()))
-					.then(QChatRoom.chatRoom.answerer.profileImageNo)
-					.otherwise(QChatRoom.chatRoom.inquirer.profileImageNo)
+					.when(chatRoom.inquirer.id.eq(member.getId()))
+					.then(chatRoom.answerer.profileImageNo)
+					.otherwise(chatRoom.inquirer.profileImageNo)
 			))
-			.from(QChatRoom.chatRoom)
-			.where(QChatRoom.chatRoom.inquirer.id.eq(member.getId())
-				.or(QChatRoom.chatRoom.answerer.id.eq(member.getId()))
-				.and(QChatRoom.chatRoom.status.eq(InquiryStatus.ACCEPTED)))
+			.from(chatRoom)
+			.where(chatRoom.inquirer.id.eq(member.getId())
+				.or(chatRoom.answerer.id.eq(member.getId())))
 			.fetch();
 
 		boolean hasNext = hasNext(pageable.getPageSize(), content);
 		return new SliceImpl<>(content, pageable, hasNext);
-	}
-
-	public Slice<ChatProposalInfo> getChatProposalsByMember(Member member, Pageable pageable){
-		List<ChatProposalInfo> content = queryFactory
-			.select(new QChatProposalInfo(
-				QChatRoom.chatRoom.id,
-				QChatRoom.chatRoom.status,
-				new CaseBuilder()
-					.when(QChatRoom.chatRoom.inquirer.id.eq(member.getId()))
-					.then(true)
-					.otherwise(false),
-				new CaseBuilder()
-					.when(QChatRoom.chatRoom.inquirer.id.eq(member.getId()))
-					.then(QChatRoom.chatRoom.answerer.id)
-					.otherwise(QChatRoom.chatRoom.inquirer.id),
-
-				new CaseBuilder()
-					.when(QChatRoom.chatRoom.inquirer.id.eq(member.getId()))
-					.then(QChatRoom.chatRoom.answerer.nickname)
-					.otherwise(QChatRoom.chatRoom.inquirer.nickname),
-				new CaseBuilder()
-					.when(QChatRoom.chatRoom.inquirer.id.eq(member.getId()))
-					.then(QChatRoom.chatRoom.answerer.jobGroup)
-					.otherwise(QChatRoom.chatRoom.inquirer.jobGroup),
-				new CaseBuilder()
-					.when(QChatRoom.chatRoom.inquirer.id.eq(member.getId()))
-					.then(QChatRoom.chatRoom.answerer.profileImageNo)
-					.otherwise(QChatRoom.chatRoom.inquirer.profileImageNo)
-			))
-			.from(QChatRoom.chatRoom)
-			.where(QChatRoom.chatRoom.inquirer.id.eq(member.getId())
-				.or(QChatRoom.chatRoom.answerer.id.eq(member.getId()))
-				.and(QChatRoom.chatRoom.status.in(List.of(InquiryStatus.REJECTED, InquiryStatus.PENDING))))
-			.fetch();
-
-		boolean hasNext = hasNext(pageable.getPageSize(), content);
-		return new SliceImpl<>(content, pageable, hasNext);
-	}
-
-	public List<Long> getAutoRejectedInquirerIds() {
-		return queryFactory
-			.select(QChatRoom.chatRoom.inquirer.id)
-			.from(QChatRoom.chatRoom)
-			.where(
-				QChatRoom.chatRoom.createdAt.loe(LocalDateTime.now().minusWeeks(1)),
-				QChatRoom.chatRoom.status.eq(InquiryStatus.PENDING)
-			)
-			.fetch();
-	}
-
-	public void updateChatRoomStatusRejected() {
-		queryFactory.update(QChatRoom.chatRoom)
-			.set(QChatRoom.chatRoom.status, InquiryStatus.REJECTED)
-			.where(
-				QChatRoom.chatRoom.createdAt.loe(LocalDateTime.now().minusWeeks(1)),
-				QChatRoom.chatRoom.status.eq(InquiryStatus.PENDING)
-			)
-			.execute();
 	}
 
 	private <T> boolean hasNext(int pageSize, List<T> items) {
