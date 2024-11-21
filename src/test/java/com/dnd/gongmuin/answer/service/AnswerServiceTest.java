a9c2c43c8107a142eb3198b6542d74c5d551e6a8
dudxo <father8475@naver.com>
2024-11-21 16:26:14 +0900

    [test] : 채택 시 크레딧 정합성 테스트

--- src/test/java/com/dnd/gongmuin/answer/service/AnswerServiceTest.java
+++ src/test/java/com/dnd/gongmuin/answer/service/AnswerServiceTest.java
@@ -4,8 +4,12 @@ import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.BDDMockito.*;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
@@ -171,5 +175,56 @@ class AnswerServiceTest {
 		assertThatThrownBy(() -> answerService.chooseAnswer(answer.getId(), notQuestioner))
 			.isInstanceOf(ValidationException.class)
 			.hasMessageContaining(QuestionPostErrorCode.NOT_AUTHORIZED.getMessage());
+	}
+
+	@DisplayName("[동시간대에 100명의 사용자에게 입금을 받는다.]")
+	@Test
+	void creditHistoryWithOneHundred() throws Exception {
+		// given
+		final long threadCount = 10_000L;
+		final int writerCredit = 10_000_000;
+		ExecutorService executorService = Executors.newFixedThreadPool(32);
+		CountDownLatch latch = new CountDownLatch((int)threadCount);
+
+		Member writer = MemberFixture.member(1L);
+		Member answer = MemberFixture.member(2L);
+		ReflectionTestUtils.setField(writer, "credit", writerCredit);
+		ReflectionTestUtils.setField(answer, "credit", 0);
+
+		List<QuestionPost> questionPosts = new ArrayList<>();
+		List<Answer> answers = new ArrayList<>();
+
+		for (long i = 1L; i <= threadCount; i++) {
+			QuestionPost questionPost = QuestionPostFixture.questionPost(i, writer);
+
+			Answer answer1 = AnswerFixture.answer(questionPost.getId(), answer);
+			ReflectionTestUtils.setField(answer1, "id", i);
+			questionPosts.add(questionPost);
+			answers.add(answer1);
+
+			given(answerRepository.findById(i)).willReturn(Optional.of(answer1));
+			given(questionPostRepository.findById(questionPost.getId())).willReturn(Optional.of(questionPost));
+		}
+
+		// when
+		long startTime = System.currentTimeMillis();
+		for (long i = 0L; i < threadCount; i++) {
+			final int index = (int)i;
+			executorService.submit(() -> {
+				try {
+					answerService.chooseAnswer(answers.get(index).getId(), writer);
+				} finally {
+					latch.countDown();
+				}
+			});
+		}
+		latch.await();
+
+		long endTime = System.currentTimeMillis();
+
+		System.out.println("Execution time: " + (endTime - startTime) + " ms");
+
+		// then
+		assertEquals(answer.getCredit(), writerCredit);
 	}
 }
