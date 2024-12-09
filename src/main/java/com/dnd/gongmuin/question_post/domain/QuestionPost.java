df6e661558ecbdda1896f44cb187c14da58ebc19
dudxo <father8475@naver.com>
2024-12-09 18:03:02 +0900

    [feat] 게시글 상태 필드 추가
    - 게시글 상태 필드 추가
    - 게시글 생성자 호출 시 '답변대기'를 기본 값으로 초기화

--- src/main/java/com/dnd/gongmuin/question_post/domain/QuestionPost.java
+++ src/main/java/com/dnd/gongmuin/question_post/domain/QuestionPost.java
@@ -56,6 +56,9 @@ public class QuestionPost extends TimeBaseEntity {
 	@Column(name = "is_chosen", nullable = false)
 	private Boolean isChosen;
 
+	@Column(name = "status", nullable = false)
+	private QuestionPostStatus questionPostStatus;
+
 	@OneToMany(mappedBy = "questionPost", cascade = CascadeType.ALL)
 	private List<QuestionPostImage> images = new ArrayList<>();
 
@@ -68,6 +71,7 @@ public class QuestionPost extends TimeBaseEntity {
 	private QuestionPost(String title, String content, int reward, JobGroup jobGroup,
 		List<QuestionPostImage> images, Member member) {
 		this.isChosen = false;
+		this.questionPostStatus = QuestionPostStatus.ANSWER_WAITING;
 		this.title = title;
 		this.content = content;
 		this.reward = reward;
