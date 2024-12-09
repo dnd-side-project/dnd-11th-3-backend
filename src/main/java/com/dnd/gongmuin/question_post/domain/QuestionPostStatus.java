8eb82df4fe1be806e521d11d34ac05050555b867
dudxo <father8475@naver.com>
2024-12-09 18:02:03 +0900

    [feat] 게시글 상태 필드 enum 작성
    - 게시글을 관리할 상태 필드값 Enum 작성
    - 답변 대기(14일 이전 답변 없음), 답변 마감(14일 이후로도 답변없음), 채택 대기(답변 있음), 채택 완료(채택 완료)

---
+++ src/main/java/com/dnd/gongmuin/question_post/domain/QuestionPostStatus.java
@@ -0,0 +1,28 @@
+package com.dnd.gongmuin.question_post.domain;
+
+import java.util.Arrays;
+
+import com.dnd.gongmuin.common.exception.runtime.NotFoundException;
+import com.dnd.gongmuin.question_post.exception.QuestionPostErrorCode;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum QuestionPostStatus {
+
+	ANSWER_WAITING("답변대기"),
+	ANSWER_CLOSE("답변마감"),
+	CHOSEN_WAITING("채택대기"),
+	CHOSEN_COMPLETE("채택완료");
+
+	private final String status;
+
+	public static QuestionPostStatus from(String status) {
+		return Arrays.stream(values())
+			.filter(questionPostStatus -> questionPostStatus.getStatus().equalsIgnoreCase(status))
+			.findFirst()
+			.orElseThrow(() -> new NotFoundException(QuestionPostErrorCode.NOT_FOUND_QUESTION_POST_STATUS));
+	}
+}
