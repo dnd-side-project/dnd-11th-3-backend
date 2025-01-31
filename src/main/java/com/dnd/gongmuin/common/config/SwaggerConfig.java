c7a8ef1f57ca07a6a9f2f5d782048d3986712c39
hs12 <loveanna1228@gmail.com>
2025-01-31 17:25:55 +0900

    [feat] : security scheme 추가

--- src/main/java/com/dnd/gongmuin/common/config/SwaggerConfig.java
+++ src/main/java/com/dnd/gongmuin/common/config/SwaggerConfig.java
@@ -1,11 +1,18 @@
 package com.dnd.gongmuin.common.config;
 
+import java.util.List;
+
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 
 @OpenAPIDefinition(
 	info = @Info(
@@ -15,6 +22,22 @@ import io.swagger.v3.oas.annotations.info.Info;
 )
 @Configuration
 public class SwaggerConfig {
+
+	@Bean
+	public OpenAPI openAPI() {
+		SecurityScheme securityScheme = new SecurityScheme()
+			.type(SecurityScheme.Type.HTTP)
+			.scheme("bearer")
+			.bearerFormat("JWT")
+			.in(SecurityScheme.In.HEADER)
+			.name(HttpHeaders.AUTHORIZATION);
+		SecurityRequirement securityRequirement
+			= new SecurityRequirement().addList(HttpHeaders.AUTHORIZATION);
+
+		return new OpenAPI()
+			.components(new Components().addSecuritySchemes(HttpHeaders.AUTHORIZATION, securityScheme))
+			.security(List.of(securityRequirement));
+	}
 
 	@Bean
 	public GroupedOpenApi chatOpenApi() {
