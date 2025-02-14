10b2cdb25b8595890c72a200ac6db3f8756b9cf6
dudxo <father8475@naver.com>
2025-02-14 16:25:35 +0900

    [feat #210] 강제 예외던지기 임시 추가

--- src/main/java/com/dnd/gongmuin/auth/controller/AuthController.java
+++ src/main/java/com/dnd/gongmuin/auth/controller/AuthController.java
@@ -73,9 +73,10 @@ public class AuthController {
 		@RequestBody @Valid AdditionalInfoRequest request,
 		@AuthenticationPrincipal Member loginMember,
 		HttpServletResponse response) {
-		SignUpResponse signUpResponse = authService.signUp(request, loginMember.getSocialEmail(), response);
+		throw new RuntimeException("Controller에서 강제 예외 발생");
+		// SignUpResponse signUpResponse = authService.signUp(request, loginMember.getSocialEmail(), response);
 
-		return ResponseEntity.ok(signUpResponse);
+		// return ResponseEntity.ok(signUpResponse);
 	}
 
 	@Operation(summary = "로그아웃 API", description = "로그아웃한다.")
