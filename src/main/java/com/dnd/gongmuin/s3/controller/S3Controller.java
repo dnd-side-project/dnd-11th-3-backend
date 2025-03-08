596f0ed06ca268932a3acd07dfcde3d0438874a8
dson220053 <dson@doeat.io>
2025-03-08 19:27:22 +0900

    fix: fix import

--- src/main/java/com/dnd/gongmuin/s3/controller/S3Controller.java
+++ src/main/java/com/dnd/gongmuin/s3/controller/S3Controller.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RequestPart;
 
 import jakarta.validation.Valid;
 
