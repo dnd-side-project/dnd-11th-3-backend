239cd1ed864e449f83e6e667a396cbeb4ae893ab
dson220053 <dson@doeat.io>
2025-03-08 19:06:15 +0900

    chore: add import

--- src/main/java/com/dnd/gongmuin/s3/controller/S3Controller.java
+++ src/main/java/com/dnd/gongmuin/s3/controller/S3Controller.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.validation.annotation.Validated;
 
