c0e64819948802109742935e5eb26d5e4a84be40
hs12 <loveanna1228@gmail.com>
2025-03-09 11:38:07 +0900

    [feat] : 이미지 업로드 API request body-> MultipartFile 입력 받도록 수정

--- src/main/java/com/dnd/gongmuin/s3/controller/S3Controller.java
+++ src/main/java/com/dnd/gongmuin/s3/controller/S3Controller.java
@@ -5,9 +5,10 @@ import java.util.List;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
-import com.dnd.gongmuin.s3.dto.ImagesUploadRequest;
 import com.dnd.gongmuin.s3.dto.ImagesUploadResponse;
 import com.dnd.gongmuin.s3.dto.VideoUploadRequest;
 import com.dnd.gongmuin.s3.dto.VideoUploadResponse;
@@ -17,6 +18,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.Size;
 import lombok.RequiredArgsConstructor;
 
 @Tag(name = "S3 API")
@@ -28,11 +30,11 @@ public class S3Controller {
 
 	@Operation(summary = "이미지 등록 API", description = "1~10장의 이미지를 등록한다.")
 	@ApiResponse(useReturnTypeSchema = true)
-	@PostMapping("/api/files/images")
+	@PostMapping(value = "/api/files/images")
 	public ResponseEntity<ImagesUploadResponse> uploadImages(
-		@ModelAttribute @Valid ImagesUploadRequest request
+		@RequestPart("imageFiles") @Size(min = 1, max = 10) List<MultipartFile> imageFiles
 	) {
-		List<String> imageUrls = s3Service.uploadImages(request.imageFiles());
+		List<String> imageUrls = s3Service.uploadImages(imageFiles);
 		return ResponseEntity.ok(ImagesUploadResponse.from(imageUrls));
 	}
 
