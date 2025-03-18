2fee728ee26217b022157d6fa8d8de7955d92201
dudxo <father8475@naver.com>
2025-03-18 16:43:51 +0900

    [feat #225] InteractionCount 낙관적 락을 위한 version 필드 추가

--- src/main/java/com/dnd/gongmuin/post_interaction/domain/InteractionCount.java
+++ src/main/java/com/dnd/gongmuin/post_interaction/domain/InteractionCount.java
@@ -11,6 +11,7 @@ import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Version;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -34,6 +35,9 @@ public class InteractionCount extends TimeBaseEntity {
 	@Enumerated(EnumType.STRING)
 	@Column(name = "type")
 	private InteractionType type;
+
+	@Version
+	private Long version;
 
 	private InteractionCount(InteractionType type, Long questionPostId) {
 		this.count = 1;
