d021a1a199d57fa7c32c06b08fe7b307f5bf5ce4
hs12 <loveanna1228@gmail.com>
2025-03-25 14:15:07 +0900

    [feat] : 채팅 에러 코드 추가

--- src/main/java/com/dnd/gongmuin/chat_inquiry/exception/ChatInquiryErrorCode.java
+++ src/main/java/com/dnd/gongmuin/chat_inquiry/exception/ChatInquiryErrorCode.java
@@ -14,7 +14,8 @@ public enum ChatInquiryErrorCode implements ErrorCode {
 	UNABLE_TO_CHANGE_STATUS("이미 수락했거나 거절한 요청입니다.", "CI_003"),
 	NOT_FOUND_STATUS("채팅방 상태값을 올바르게 입력해주세요.", "CI_004"),
 	NOT_EXISTS_ANSWERER("해당 아이디의 답변자가 해당 게시글에 존재하지 않습니다.", "CI_005"),
-	SELF_INQUIRY_NOT_ALLOWED("자기 자신에게 채팅 요청을 보낼 수 없습니다.", "CI_006");
+	SELF_INQUIRY_NOT_ALLOWED("자기 자신에게 채팅 요청을 보낼 수 없습니다.", "CI_006"),
+	ALREADY_REQUESTED("이미 채팅 요청을 신청했습니다.", "CI_007");
 
 	private final String message;
 	private final String code;
